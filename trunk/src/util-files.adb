ccdb5780b1ceda9a1c9bf42b0f4d3ea87d15e666
Stephane.Carrez <Stephane.Carrez@2d814c7b-9a8a-7a73-480f-6df5a50947db>
2010-05-14 13:33:48 +0000

    Integrate the file utilities - read and write files into a string

---
+++ trunk/src/util-files.adb
@@ -0,0 +1,109 @@
+-----------------------------------------------------------------------
+--  Util.Files -- Various File Utility Packages
+--  Copyright (C) 2001, 2002, 2003, 2009, 2010 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+with Ada.Directories;
+with Ada.Streams;
+with Ada.Streams.Stream_IO;
+package body Util.Files is
+
+   --  ------------------------------
+   --  Read a complete file into a string.
+   --  The <b>Max_Size</b> parameter indicates the maximum size that is read.
+   --  ------------------------------
+   procedure Read_File (Path     : in String;
+                        Into     : out Unbounded_String;
+                        Max_Size : in Natural := 0) is
+      use Ada.Streams;
+      use Ada.Streams.Stream_IO;
+
+      F      : File_Type;
+      Buffer : Stream_Element_Array (1 .. 10_000);
+      Pos    : Positive_Count := 1;
+      Last   : Stream_Element_Offset;
+      Space  : Natural;
+   begin
+      if Max_Size = 0 then
+         Space := Natural'Last;
+      else
+         Space := Max_Size;
+      end if;
+      Open (Name => Path, File => F, Mode => In_File);
+      loop
+         Read (File => F, Item => Buffer, From => Pos, Last => Last);
+         if Natural (Last) > Space then
+            Last := Stream_Element_Offset (Space);
+         end if;
+         for I in 1 .. Last loop
+            Append (Into, Character'Val (Buffer (I)));
+         end loop;
+         exit when Last < Buffer'Length;
+         Pos := Pos + Positive_Count (Last);
+      end loop;
+      Close (F);
+
+   exception
+      when others =>
+         if Is_Open (F) then
+            Close (F);
+         end if;
+         raise;
+   end Read_File;
+
+   --  ------------------------------
+   --  Save the string into a file creating the file if necessary
+   --  ------------------------------
+   procedure Write_File (Path    : in String;
+                         Content : in String) is
+      use Ada.Streams;
+      use Ada.Streams.Stream_IO;
+      use Ada.Directories;
+
+      F : File_Type;
+      Buffer : Stream_Element_Array (Stream_Element_Offset (Content'First)
+                                     .. Stream_Element_Offset (Content'Last));
+
+      Dir : constant String := Containing_Directory (Path);
+   begin
+      if not Exists (Dir) then
+         Create_Path (Dir);
+      end if;
+      Create (File => F, Name => Path);
+      for I in Content'Range loop
+         Buffer (Stream_Element_Offset (I))
+           := Stream_Element (Character'Pos (Content (I)));
+      end loop;
+      Write (F, Buffer);
+      Close (F);
+
+   exception
+      when others =>
+         if Is_Open (F) then
+            Close (F);
+         end if;
+         raise;
+   end Write_File;
+
+   --  ------------------------------
+   --  Save the string into a file creating the file if necessary
+   --  ------------------------------
+   procedure Write_File (Path    : in String;
+                         Content : in Unbounded_String) is
+   begin
+      Write_File (Path, Ada.Strings.Unbounded.To_String (Content));
+   end Write_File;
+
+end Util.Files;
---
+++ trunk/src/util-files.ads
@@ -0,0 +1,37 @@
+-----------------------------------------------------------------------
+--  Util.Files -- Various File Utility Packages
+--  Copyright (C) 2001, 2002, 2003, 2009, 2010 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+with Ada.Strings.Unbounded;
+package Util.Files is
+
+   use Ada.Strings.Unbounded;
+
+   --  Read a complete file into a string.
+   --  The <b>Max_Size</b> parameter indicates the maximum size that is read.
+   procedure Read_File (Path     : in String;
+                        Into     : out Unbounded_String;
+                        Max_Size : in Natural := 0);
+
+   --  Save the string into a file creating the file if necessary
+   procedure Write_File (Path    : in String;
+                         Content : in String);
+
+   --  Save the string into a file creating the file if necessary
+   procedure Write_File (Path    : in String;
+                         Content : in Unbounded_String);
+
+end Util.Files;
