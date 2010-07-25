67e6368f7d300b8ec0749a4057f718d83c5abee7
Stephane.Carrez <Stephane.Carrez@2d814c7b-9a8a-7a73-480f-6df5a50947db>
2010-07-25 17:45:32 +0000

    Add read-write locks (multiple readers/single writer lock)

---
+++ src/util-concurrent-locks.ads
@@ -0,0 +1,39 @@
+-----------------------------------------------------------------------
+--  Util.Concurrent.Locks -- Concurrent Tools
+--  Copyright (C) 2001, 2002, 2003, 2009, 2010 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+package Util.Concurrent.Locks is
+
+   --  Lock for accessing the shared cache
+   protected type RW_Lock is
+      --  Lock the resource for reading.
+      entry Read;
+
+      --  Release the read lock.
+      procedure Release_Read;
+
+      --  Lock the resource for writing.
+      entry Write;
+
+      --  Release the write lock.
+      procedure Release_Write;
+
+   private
+      Readable     : Boolean := True;
+      Reader_Count : Natural := 0;
+   end RW_Lock;
+
+end Util.Concurrent.Locks;
