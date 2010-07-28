9bc47125a2cfc3babd53aa13f6e39ef08c3fa3c2
Stephane.Carrez <Stephane.Carrez@2d814c7b-9a8a-7a73-480f-6df5a50947db>
2010-07-28 20:16:28 +0000

    Add support to escape a string using various rules (Java, Javascript, Sql, XML) - provide a generic implementation that can be used with a specific   character type (Character, Wide_Character, ...) and a specific appender - provide an instantiation for string and wide_wide_string

--- src/util-strings.adb
+++ src/util-strings.adb
@@ -17,6 +17,7 @@
 -----------------------------------------------------------------------
 
 with Ada.Strings.Hash;
+with Ada.Unchecked_Deallocation;
 package body Util.Strings is
 
    --  ------------------------------
@@ -37,5 +38,135 @@ package body Util.Strings is
       end if;
       return Left.all = Right.all;
    end Equivalent_Keys;
+
+
+   use Util.Concurrent.Counters;
+   function To_String_Ref (S : in String) return String_Ref is
+      Str : constant String_Record_Access
+        := new String_Record '(Len => S'Length, Str => S, Counter => ONE);
+   begin
+       return String_Ref '(Ada.Finalization.Controlled with
+                           Str => Str);
+   end To_String_Ref;
+
+   function To_String (S : in String_Ref) return String is
+   begin
+      return S.Str.Str;
+   end To_String;
+
+   overriding
+   procedure Adjust (Object : in out String_Ref) is
+   begin
+      if Object.Str /= null then
+          Util.Concurrent.Counters.Increment (Object.Str.Counter);
+      end if;
+   end Adjust;
+
+   overriding
+   procedure Finalize (Object : in out String_Ref) is
+
+     procedure Free is
+        new Ada.Unchecked_Deallocation (String_Record, String_Record_Access);
+
+      Is_Zero : Boolean;
+   begin
+      if Object.Str /= null then
+          Util.Concurrent.Counters.Decrement (Object.Str.Counter, Is_Zero);
+          if Is_Zero then
+              Free (Object.Str);
+          end if;
+      end if;
+   end Finalize;
+
+   package body Escape is
+      type Code is mod 2**32;
+
+      procedure Put_Hex (Into : in out Stream; Value : Code) is
+         Conversion : constant String (1 .. 16) := "0123456789ABCDEF";
+         S : String (1 .. 6) := (1 => '\', 2 => 'u', others => '0');
+         P : Code := Value;
+         N : Code;
+         I : Positive := S'Last;
+      begin
+         while P /= 0 loop
+            N := P mod 16;
+            P := P / 16;
+            S (I) := Conversion (Positive'Val (N + 1));
+            exit when I = 1;
+            I := I - 1;
+         end loop;
+         for J in S'Range loop
+            Put (Into, S (J));
+         end loop;
+      end Put_Hex;
+
+      procedure Escape_Java_Script (Content : in Input;
+                                    Into    : in out Stream) is
+      begin
+         Escape_Java (Content => Content, Into => Into,
+                      Escape_Single_Quote => True);
+      end Escape_Java_Script;
+
+      procedure Escape_Java (Content : in Input;
+                             Into    : in out Stream) is
+      begin
+         Escape_Java (Content => Content, Into => Into,
+                      Escape_Single_Quote => False);
+      end Escape_Java;
+
+      procedure Escape_Java (Content : in Input;
+                             Escape_Single_Quote : in Boolean;
+                             Into : in out Stream) is
+         C : Code;
+      begin
+         for I in Content'Range loop
+            C := Char'Pos (Content (I));
+            if C < 16#20# then
+               if C = 16#0A# then
+                  Put (Into, '\');
+                  Put (Into, 'n');
+
+               elsif C = 16#0D# then
+                  Put (Into, '\');
+                  Put (Into, 'r');
+
+               elsif C = 16#08# then
+                  Put (Into, '\');
+                  Put (Into, 'b');
+
+               elsif C = 16#09# then
+                  Put (Into, '\');
+                  Put (Into, 't');
+
+               elsif C = 16#0C# then
+                  Put (Into, '\');
+                  Put (Into, 'f');
+               else
+                  Put_Hex (Into, C);
+               end if;
+
+            elsif C = 16#27# then
+               if Escape_Single_Quote then
+                  Put (Into, '\');
+               end if;
+               Put (Into, Character'Val (C));
+
+            elsif C = 16#22# then
+               Put (Into, '\');
+               Put (Into, Character'Val (C));
+
+            elsif C = 16#5C# then
+               Put (Into, '\');
+               Put (Into, Character'Val (C));
+
+            elsif C > 16#80# then
+               Put_Hex (Into, C);
+
+            else
+               Put (Into, Character'Val (C));
+            end if;
+         end loop;
+      end Escape_Java;
+   end Escape;
 
 end Util.Strings;
