9e0f758c27e38304ff27da83be747a5d6e4a1ece
Stephane.Carrez@gmail.com <Stephane.Carrez@gmail.com@2d814c7b-9a8a-7a73-480f-6df5a50947db>
2010-08-14 15:31:38 +0000

    Add unit test for string capitalize, upper case, lower case

--- regtests/util-strings-tests.adb
+++ regtests/util-strings-tests.adb
@@ -21,20 +21,28 @@ with AUnit.Assertions;
 with AUnit.Test_Caller;
 with Util.Tests;
 with Ada.Strings.Unbounded;
-with Util.Strings.Escapes;
+--  with Util.Strings.Escapes;
+with Util.Strings.Transforms;
 package body Util.Strings.Tests is
 
    use Ada.Strings.Unbounded;
    use Util.Tests;
-   use Util.Strings.Escapes;
+--     use Util.Strings.Escapes;
+   use Util.Strings.Transforms;
    use AUnit.Assertions;
 
    package Caller is new AUnit.Test_Caller (Test);
 
    procedure Add_Tests (Suite : AUnit.Test_Suites.Access_Test_Suite) is
    begin
-      Suite.Add_Test (Caller.Create ("Test Util.Strings.Escapes.Escape_Javascript",
+      Suite.Add_Test (Caller.Create ("Test Util.Strings.Transforms.Escape_Javascript",
         Test_Escape_Javascript'Access));
+      Suite.Add_Test (Caller.Create ("Test Util.Strings.Transforms.Capitalize",
+        Test_Capitalize'Access));
+      Suite.Add_Test (Caller.Create ("Test Util.Strings.Transforms.To_Upper_Case",
+        Test_To_Upper_Case'Access));
+      Suite.Add_Test (Caller.Create ("Test Util.Strings.Transforms.To_Lower_Case",
+        Test_To_Lower_Case'Access));
    end Add_Tests;
 
    procedure Test_Escape_Javascript (T : in out Test) is
@@ -51,5 +59,32 @@ package body Util.Strings.Tests is
 			 Into    => Result);
       Assert_Equals ("\u001B[m \u00FF", Result);
    end Test_Escape_Javascript;
+
+   procedure Test_Capitalize (T : in out Test) is
+      pragma Unreferenced (T);
+
+      Result : Unbounded_String;
+   begin
+      Assert_Equals ("Capitalize_A_String", Capitalize ("capITalIZe_a_strING"));
+
+      Capitalize ("CapAS_String", Result);
+      Assert_Equals ("Capas_String", Result);
+   end Test_Capitalize;
+
+   procedure Test_To_Upper_Case (T : in out Test) is
+      pragma Unreferenced (T);
+
+      Result : Unbounded_String;
+   begin
+      Assert_Equals ("UPPERCASE_0123_STR", To_Upper_Case ("upperCase_0123_str"));
+   end Test_To_Upper_Case;
+
+   procedure Test_To_Lower_Case (T : in out Test) is
+      pragma Unreferenced (T);
+
+      Result : Unbounded_String;
+   begin
+      Assert_Equals ("lowercase_0123_str", To_Lower_Case ("LowERCase_0123_STR"));
+   end Test_To_Lower_Case;
 
 end Util.Strings.Tests;
