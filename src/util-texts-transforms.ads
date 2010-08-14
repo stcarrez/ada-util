771e6b1963cccb09006b9de4952f14aa9defa6dd
Stephane.Carrez@gmail.com <Stephane.Carrez@gmail.com@2d814c7b-9a8a-7a73-480f-6df5a50947db>
2010-08-14 15:28:15 +0000

    Generic text transformation package - capitalize, upper case, lower case conversion - escape javascript

---
+++ src/util-texts-transforms.ads
@@ -0,0 +1,63 @@
+-----------------------------------------------------------------------
+--  Util-texts -- Various Text Transformation Utilities
+--  Copyright (C) 2001, 2002, 2003, 2009, 2010 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+generic
+   type Stream is limited private;
+   type Char is (<>);
+   type Input is array (Positive range <>) of Char;
+   with procedure Put (Buffer : in out Stream; C : in Character);
+   with function To_Upper (C : Char) return Char;
+   with function To_Lower (C : Char) return Char;
+   with function To_Input (S : Stream) return Input;
+package Util.Texts.Transforms is
+
+   --  Capitalize the string into the result stream.
+   procedure Capitalize (Content : in Input;
+                         Into    : in out Stream);
+
+   --  Capitalize the string
+   function Capitalize (Content : Input) return Input;
+
+   --  Translate the input string into an upper case string in the result stream.
+   procedure To_Upper_Case (Content : in Input;
+                            Into    : in out Stream);
+
+   --  Translate the input string into an upper case string.
+   function To_Upper_Case (Content : Input) return Input;
+
+   --  Translate the input string into a lower case string in the result stream.
+   procedure To_Lower_Case (Content : in Input;
+                            Into    : in out Stream);
+   function To_Lower_Case (Content : Input) return Input;
+
+   --  Escape the content into the result stream using the JavaScript
+   --  escape rules.
+   procedure Escape_Java_Script (Content : in Input;
+                                 Into    : in out Stream);
+   function Escape_Java_Script (Content : Input) return Input;
+
+   --  Escape the content into the result stream using the Java
+   --  escape rules.
+   procedure Escape_Java (Content : in Input;
+                          Into    : in out Stream);
+   function Escape_Java (Content : Input) return Input;
+
+private
+   procedure Escape_Java (Content             : in Input;
+                          Escape_Single_Quote : in Boolean;
+                          Into                : in out Stream);
+end Util.Texts.Transforms;
