c5e53a3b3c8c60928022e9357ebd2c284221427b
Stephane.Carrez <Stephane.Carrez@2d814c7b-9a8a-7a73-480f-6df5a50947db>
2010-08-23 22:07:12 +0000

    Add simple example for text transformations

---
+++ trunk/samples/escape.adb
@@ -0,0 +1,43 @@
+-----------------------------------------------------------------------
+--  escape -- Text Transformations
+--  Copyright (C) 2001, 2002, 2003, 2006, 2008, 2009, 2010 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+with Util.Strings.Transforms;
+with Ada.Strings.Unbounded;
+with Ada.Text_IO;
+with Ada.Command_Line;
+
+procedure Escape is
+
+   use Ada.Strings.Unbounded;
+
+   Count : constant Natural := Ada.Command_Line.Argument_Count;
+begin
+   if Count = 0 then
+      Ada.Text_IO.Put_Line ("Usage: escape string...");
+      return;
+   end if;
+   for I in 1 .. Count loop
+      declare
+         S : constant String := Ada.Command_Line.Argument (I);
+      begin
+         Ada.Text_IO.Put_Line ("Escape javascript : "
+                               & Util.Strings.Transforms.Escape_Javascript (S));
+         Ada.Text_IO.Put_Line ("Escape XML        : "
+                               & Util.Strings.Transforms.Escape_Xml (S));
+      end;
+   end loop;
+end Escape;
