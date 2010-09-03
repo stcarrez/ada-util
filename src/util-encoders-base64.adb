d4423ff057dc0394d9a6bfbf12d17c64fdcd72e0
Stephane.Carrez <Stephane.Carrez@2d814c7b-9a8a-7a73-480f-6df5a50947db>
2010-09-03 20:54:31 +0000

    Add support for stream encoders and decoders Add a Base64 encoder/decoder Add a Base16 (hex) encoder/decoder

---
+++ src/util-encoders-base64.adb
@@ -0,0 +1,218 @@
+-----------------------------------------------------------------------
+--  util-encoders-base64 -- Encode/Decode a stream in base64adecimal
+--  Copyright (C) 2009, 2010 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+with Ada.Unchecked_Deallocation;
+package body Util.Encoders.Base64 is
+
+   use Interfaces;
+   use Ada;
+
+   use type Ada.Streams.Stream_Element_Offset;
+
+   --  ------------------------------
+   --  Encodes the binary input stream represented by <b>Data</b> into
+   --  the a base64 output stream <b>Into</b>.
+   --
+   --  If the transformer does not have enough room to write the result,
+   --  it must return in <b>Encoded</b> the index of the last encoded
+   --  position in the <b>Data</b> stream.
+   --
+   --  The transformer returns in <b>Last</b> the last valid position
+   --  in the output stream <b>Into</b>.
+   --
+   --  The <b>Encoding_Error</b> exception is raised if the input
+   --  stream cannot be transformed.
+   --  ------------------------------
+   overriding
+   procedure Transform (E       : in Encoder;
+                        Data    : in Ada.Streams.Stream_Element_Array;
+                        Into    : out Ada.Streams.Stream_Element_Array;
+                        Last    : out Ada.Streams.Stream_Element_Offset;
+                        Encoded : out Ada.Streams.Stream_Element_Offset) is
+
+      Pos      : Streams.Stream_Element_Offset := Into'First;
+      I        : Streams.Stream_Element_OFfset := Data'First;
+      C1, C2   : Unsigned_8;
+
+      Alphabet : constant Alphabet_Access := E.Alphabet;
+
+   begin
+      while I <= Data'Last loop
+         if Pos + 4 > Into'Last + 1 then
+            Last    := Pos - 1;
+            Encoded := I - 1;
+            return;
+         end if;
+
+         --  Encode the first byte, add padding if necessary.
+         C1 := Unsigned_8 (Data (I));
+         Into (Pos) := Alphabet (Shift_Right (C1, 2));
+         if I = Data'Last then
+            Into (Pos + 1) := Alphabet (Shift_Left (C1 and 3, 4));
+            Into (Pos + 2) := Character'Pos ('=');
+            Into (Pos + 3) := Character'Pos ('=');
+            Last := Pos + 3;
+            Encoded := Data'Last;
+            return;
+         end if;
+
+         --  Encode the second byte, add padding if necessary.
+         C2 := Unsigned_8 (Data (I + 1));
+         Into (Pos + 1) := Alphabet (Shift_Left (C1 and 16#03#, 4) or Shift_Right (C2, 4));
+         if I = Data'Last - 1 then
+            Into (Pos + 2) := Alphabet (Shift_Left (C2 and 16#0F#, 2));
+            Into (Pos + 3) := Character'Pos ('=');
+            Last := Pos + 3;
+            Encoded := Data'Last;
+            return;
+         end if;
+
+         --  Encode the third byte
+         C1 := Unsigned_8 (Data (I + 2));
+         Into (Pos + 2) := Alphabet (Shift_Left (C2 and 16#0F#, 2) or Shift_Right (C1, 6));
+         Into (Pos + 3) := Alphabet (C1 and 16#03F#);
+         Pos := Pos + 4;
+         I   := I + 3;
+      end loop;
+      Last    := Pos - 1;
+      Encoded := Data'Last;
+   end Transform;
+
+   --  ------------------------------
+   --  Delete the encoder object.
+   --  ------------------------------
+   overriding
+   procedure Delete (E : access Encoder) is
+
+     type Encoder_Access is access all Encoder'Class;
+
+     procedure Free is
+       new Ada.Unchecked_Deallocation (Encoder'Class, Encoder_Access);
+
+     O : Encoder_Access := E.all'Access;
+   begin
+      Free (O);
+   end Delete;
+
+   --  ------------------------------
+   --  Decodes the base64 input stream represented by <b>Data</b> into
+   --  the binary output stream <b>Into</b>.
+   --
+   --  If the transformer does not have enough room to write the result,
+   --  it must return in <b>Encoded</b> the index of the last encoded
+   --  position in the <b>Data</b> stream.
+   --
+   --  The transformer returns in <b>Last</b> the last valid position
+   --  in the output stream <b>Into</b>.
+   --
+   --  The <b>Encoding_Error</b> exception is raised if the input
+   --  stream cannot be transformed.
+   --  ------------------------------
+   overriding
+   procedure Transform (E       : in Decoder;
+                        Data    : in Ada.Streams.Stream_Element_Array;
+                        Into    : out Ada.Streams.Stream_Element_Array;
+                        Last    : out Ada.Streams.Stream_Element_Offset;
+                        Encoded : out Ada.Streams.Stream_Element_Offset) is
+
+     use Ada.Streams;
+
+      Size     : constant Streams.Stream_Element_Offset := Data'Length / 4;
+      Pos      : Streams.Stream_Element_Offset := Into'First;
+      I        : Streams.Stream_Element_Offset := Data'First;
+      C1, C2   : Streams.Stream_Element;
+      Val1, Val2 : Unsigned_8;
+
+      Values : constant Alphabet_Values_Access := E.Values;
+
+   begin
+      if Data'Length /= Size * 4 then
+         raise Encoding_Error with "Invalid input stream length";
+      end if;
+      while I <= Data'Last loop
+         if Pos + 3 > Into'Last + 1 then
+            Last    := Pos - 1;
+            Encoded := I - 1;
+            return;
+         end if;
+
+         --  Decode the first two bytes to produce the first output byte
+         C1 := Data (I);
+         Val1 := Values (C1);
+         if (Val1 and 16#C0#) /= 0 then
+            raise Encoding_Error with "Invalid character '" & Character'Val (C1) & "'";
+         end if;
+
+         C2 := Data (I + 1);
+         Val2 := Values (C2);
+         if (Val2 and 16#C0#) /= 0 then
+            raise Encoding_Error with "Invalid character '" & Character'Val (C2) & "'";
+         end if;
+
+         Into (Pos) := Stream_Element (Shift_Left (Val1, 2) or Shift_Right (Val2, 4));
+
+         --  Decode the next byte
+         C1 := Data (I + 2);
+         Val1 := Values (C1);
+         if (Val1 and 16#C0#) /= 0 then
+            if C1 /= Character'Pos ('=') then
+               raise Encoding_Error with "Invalid character '" & Character'Val (C1) & "'";
+            end if;
+            Encoded := I + 3;
+            Last    := Pos;
+            return;
+         end if;
+
+         Into (Pos + 1) := Stream_Element (Shift_Left (Val2, 4) or Shift_Right (Val1, 2));
+
+         C2 := Data (I + 3);
+         Val2 := Values (C2);
+         if (Val2 and 16#C0#) /= 0 then
+            if C2 /= Character'Pos ('=') then
+               raise Encoding_Error with "Invalid character '" & Character'Val (C1) & "'";
+            end if;
+            Encoded := I + 3;
+            Last    := Pos + 1;
+            return;
+         end if;
+
+         Into (Pos + 2) := Stream_Element (Shift_Left (Val1, 6) or Val2);
+         Pos := Pos + 3;
+         I   := I + 4;
+      end loop;
+      Last    := Pos - 1;
+      Encoded := Data'Last;
+   end Transform;
+
+   --  ------------------------------
+   --  Delete the decoder object.
+   --  ------------------------------
+   overriding
+   procedure Delete (E : access Decoder) is
+
+     type Decoder_Access is access all Decoder'Class;
+
+     procedure Free is
+       new Ada.Unchecked_Deallocation (Decoder'Class, Decoder_Access);
+
+     O : Decoder_Access := E.all'Access;
+   begin
+      Free (O);
+   end Delete;
+
+end Util.Encoders.Base64;
---
+++ src/util-encoders.ads
@@ -0,0 +1,132 @@
+-----------------------------------------------------------------------
+--  util-encoders -- Encode/Decode streams and strings from one format to another
+--  Copyright (C) 2009, 2010 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+with Ada.Streams;
+with Ada.Finalization;
+with Ada.Strings.Unbounded;
+
+--  The <b>Util.Encoders</b> package defines the <b>Encoder</b> object
+--  which represents a mechanism to transform a stream from one format into
+--  another format.
+package Util.Encoders is
+
+   Not_Supported  : exception;
+   Encoding_Error : exception;
+
+   --  Encoder/decoder for Base64 (RFC 4648)
+   BASE_64     : constant String := "base64";
+
+   --  Encoder/decoder for Base64 (RFC 4648) using the URL alphabet
+   --  (+ and / are replaced by - and _)
+   BASE_64_URL : constant String := "base64url";
+
+   --  Encoder/decoder for Base16 (RFC 4648)
+   BASE_16     : constant String := "base16";
+   HEX         : constant String := "hex";
+
+   --  ------------------------------
+   --  Encoder context object
+   --  ------------------------------
+   --  The <b>Encoder</b> provides operations to encode and decode
+   --  strings or stream of data from one format to another.
+   --  The <b>Encoded</b> contains two <b>Transformer</b>
+   --  which either <i>encodes</i> or <i>decodes</i> the stream.
+   type Encoder is tagged limited private;
+
+   --  Encodes the input string <b>Data</b> using the transformation
+   --  rules provided by the <b>E</b> encoder.
+   --
+   --  Returns the encoded string.
+   --
+   --  Raises the <b>Encoding_Error</b> exception if the source string
+   --  cannot be encoded.
+   --  Raises the <b>Not_Supported</b> exception if the encoding is not
+   --  supported.
+   function Encode (E    : in Encoder;
+                    Data : in String) return String;
+
+   --  Decodes the input string <b>Data</b> using the transformation
+   --  rules provided by the <b>E</b> encoder.
+   --
+   --  Returns the encoded string.
+   --
+   --  Raises the <b>Encoding_Error</b> exception if the source string
+   --  cannot be decoded.
+   --  Raises the <b>Not_Supported</b> exception if the decoding is not
+   --  supported.
+   function Decode (E    : in Encoder;
+                    Data : in String) return String;
+
+   --  Create the encoder object for the specified encoding format.
+   function Create (Name : String) return Encoder;
+
+   --  ------------------------------
+   --  Stream Transformation interface
+   --  ------------------------------
+   --  The <b>Transformer</b> interface defines the operation to transform
+   --  a stream from one data format to another.
+   type Transformer is interface;
+   type Transformer_Access is access all Transformer'Class;
+
+   --  Transform the input stream represented by <b>Data</b> into
+   --  the output stream <b>Into</b>.  The transformation made by
+   --  the object can be of any nature (Hex encoding, Base64 encoding,
+   --  Hex decoding, Base64 decoding, ...).
+   --
+   --  If the transformer does not have enough room to write the result,
+   --  it must return in <b>Encoded</b> the index of the last encoded
+   --  position in the <b>Data</b> stream.
+   --
+   --  The transformer returns in <b>Last</b> the last valid position
+   --  in the output stream <b>Into</b>.
+   --
+   --  The <b>Encoding_Error</b> exception is raised if the input
+   --  stream cannot be transformed.
+   procedure Transform (E       : in Transformer;
+                        Data    : in Ada.Streams.Stream_Element_Array;
+                        Into    : out Ada.Streams.Stream_Element_Array;
+                        Last    : out Ada.Streams.Stream_Element_Offset;
+                        Encoded : out Ada.Streams.Stream_Element_Offset) is abstract;
+
+   --  Delete the transformer object.
+   procedure Delete (E : access Transformer) is abstract;
+
+   procedure Transform (E    : in Transformer;
+                        Data : in String;
+                        Result : out Ada.Strings.Unbounded.Unbounded_String) is null;
+
+   --  Transform the input string <b>Data</b> using the transformation
+   --  rules provided by the <b>E</b> transformer.
+   --
+   --  Returns the transformed string.
+   --
+   --  Raises the <b>Encoding_Error</b> exception if the source string
+   --  cannot be transformed
+   function Transform (E    : in Transformer'Class;
+                       Data : in String) return String;
+
+private
+
+   type Encoder is new Ada.Finalization.Limited_Controlled with record
+      Encode : Transformer_Access;
+      Decode : Transformer_Access;
+   end record;
+
+   --  Delete the transformers
+   procedure Finalize (E : in out Encoder);
+
+end Util.Encoders;
