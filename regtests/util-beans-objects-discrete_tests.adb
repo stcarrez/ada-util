df33150a30a73640009b75ba0b2140d7741d399c
Stephane.Carrez <Stephane.Carrez@2d814c7b-9a8a-7a73-480f-6df5a50947db>
2011-01-08 16:57:10 +0000

    Integrate the EL.Objects, EL.Beans framework from Ada EL (See http://code.google.com/p/ada-el/) - Integrate the unit tests from Ada EL

---
+++ regtests/util-beans-objects-discrete_tests.adb
@@ -0,0 +1,266 @@
+-----------------------------------------------------------------------
+--  Util.Beans.Objects.Discrete_Tests - Generic simple test for discrete object types
+--  Copyright (C) 2009, 2010 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+with Ada.Containers;
+with AUnit.Test_Caller;
+with Ada.Strings.Fixed;
+with Ada.Text_IO;
+with Ada.Calendar;
+with Util.Beans.Objects.Hash;
+package body Util.Beans.Objects.Discrete_Tests is
+
+   use Ada.Strings.Fixed;
+   use Ada.Containers;
+
+   procedure Test_Eq (T : Test; V : String; N : Test_Type);
+   procedure Test_Conversion (T : Test; V : String; N : Test_Type);
+   procedure Test_Lt_Gt (T : Test; V : String; N : Test_Type);
+   procedure Test_Sub (T : Test; V : String; N : Test_Type);
+   procedure Test_Add (T : Test; V : String; N : Test_Type);
+
+   --  Generic test for To_Object and To_XXX types
+   --  Several values are specified in the Test_Values string.
+   generic
+      with procedure Basic_Test (T : in Test; V : String; N : Test_Type);
+   procedure Test_Basic_Object (T : in out Test);
+
+   procedure Test_Basic_Object (T : in out Test) is
+      pragma Unmodified (T);
+
+      Pos, Next : Natural;
+   begin
+      Pos := Test_Values'First;
+      while Pos <= Test_Values'Last loop
+         Next := Index (Test_Values, ",", Pos);
+         if Next < Pos then
+            Next := Test_Values'Last + 1;
+         end if;
+         declare
+            V : constant String := Test_Values (Pos .. Next - 1);
+            N : constant Test_Type := Value (V);
+         begin
+            Basic_Test (T, V, N);
+         end;
+         Pos := Next + 1;
+      end loop;
+   end Test_Basic_Object;
+
+   --  ------------------------------
+   --  Test Util.Beans.Objects.To_Object
+   --  ------------------------------
+   procedure Test_Conversion (T : Test; V : String; N : Test_Type) is
+      Value : Util.Beans.Objects.Object;
+   begin
+      Value := To_Object (V);
+      T.Assert (Condition => To_Type (Value) = N,
+                Message   => Test_Name & " returned invalid value: "
+                & To_String (Value) & " when we expected: " & V);
+
+      T.Assert (Condition => V = To_String (Value),
+                Message   => Test_Name & ".To_String returned invalid value: "
+                & To_String (Value) & " when we expected: " & V);
+   end Test_Conversion;
+   procedure Test_To_Object is new Test_Basic_Object (Basic_Test => Test_Conversion);
+
+   --  ------------------------------
+   --  Test Util.Beans.Objects.Hash
+   --  ------------------------------
+    procedure Test_Hash (T : in out Test) is
+      pragma Unmodified (T);
+
+      Pos, Next   : Natural;
+      Hash_Values : array (Test_Values'Range) of Hash_Type := (others => 0);
+      Nb_Hash     : Natural := 0;
+   begin
+      Pos := Test_Values'First;
+      while Pos <= Test_Values'Last loop
+         Next := Index (Test_Values, ",", Pos);
+         if Next < Pos then
+            Next := Test_Values'Last + 1;
+         end if;
+         declare
+            V     : constant String := Test_Values (Pos .. Next - 1);
+            N     : constant Test_Type := Value (V);
+            Value : constant Util.Beans.Objects.Object := To_Object_Test (N);
+            H     : constant Hash_Type := Util.Beans.Objects.Hash (Value);
+            Found : Boolean := False;
+         begin
+            for J in 1 .. Nb_Hash loop
+               if Hash_Values (J) = H then
+                  Found := True;
+               end if;
+            end loop;
+            if not Found then
+               Nb_Hash := Nb_Hash + 1;
+               Hash_Values (Nb_Hash) := H;
+            end if;
+         end;
+         Pos := Next + 1;
+      end loop;
+
+      Ada.Text_IO.Put_Line ("Found " & Natural'Image (Nb_Hash) & " hash values");
+      Assert (T, Nb_Hash > 1, "Only one hash value found");
+   end Test_Hash;
+
+   --  ------------------------------
+   --  Test Util.Beans.Objects."+"
+   --  ------------------------------
+   procedure Test_Add (T : Test; V : String; N : Test_Type) is
+      Value : Util.Beans.Objects.Object := To_Object_Test (N);
+   begin
+      Value := Value + To_Object_Test (N);
+      T.Assert (Condition => To_Type (Value) = N + N,
+                Message   => Test_Name & " returned invalid value: "
+                & To_String (Value) & " when we expected: " & V);
+   end Test_Add;
+
+   procedure Test_Add is new Test_Basic_Object (Test_Add);
+
+   --  ------------------------------
+   --  Test Util.Beans.Objects."-"
+   --  ------------------------------
+   procedure Test_Sub (T : Test; V : String; N : Test_Type) is
+      pragma Unreferenced (V);
+
+      Value : Util.Beans.Objects.Object;
+   begin
+      Value := To_Object_Test (N) - To_Object_Test (N);
+      T.Assert (Condition => To_Type (Value) = N - N,
+                Message   => Test_Name & " returned invalid value: "
+                & To_String (Value) & " when we expected: 0");
+   end Test_Sub;
+
+   procedure Test_Sub is new Test_Basic_Object (Test_Sub);
+
+   --  ------------------------------
+   --  Test Util.Beans.Objects."<" and Util.Beans.Objects.">"
+   --  ------------------------------
+   procedure Test_Lt_Gt (T : Test; V : String; N : Test_Type) is
+      Res   : Boolean;
+      Is_Neg : constant Boolean := Index (V, "-") = V'First;
+      O : Util.Beans.Objects.Object := To_Object_Test (N);
+   begin
+      Res := To_Object_Test (N) < To_Object_Test (N);
+      T.Assert (Condition => Res = False,
+                Message   => Test_Name & ".'<' returned invalid value: "
+                & Boolean'Image (Res) & " when we expected: false");
+      Res := To_Object_Test (N) > To_Object_Test (N);
+      T.Assert (Condition => Res = False,
+                Message   => Test_Name & ".'>' returned invalid value: "
+                & Boolean'Image (Res) & " when we expected: false");
+      Res := To_Object_Test (N) + To_Object_Test (N) < To_Object_Test (N);
+      T.Assert (Condition => Res = Is_Neg,
+                Message   => Test_Name & ".'<' returned invalid value: "
+                & Boolean'Image (Res) & " when we expected: "
+                & Boolean'Image (Is_Neg)
+                & " with value: " & V & "Num=" & Long_Long_Integer'Image (To_Long_Long_Integer (O))
+                & " Sum=" & Long_Long_Integer'Image (To_Long_Long_Integer (O + O)));
+      Res := To_Object_Test (N) > To_Object_Test (N) + To_Object_Test (N);
+      T.Assert (Condition => Res = Is_Neg,
+                Message   => Test_Name & ".'>' returned invalid value: "
+                & Boolean'Image (Res) & " when we expected: "
+                & Boolean'Image (Is_Neg)
+                & " with value: " & V);
+      if V /= "0" and V /= "false" and V /= "true" then
+         Res := To_Object_Test (N) < To_Object_Test (N) + To_Object_Test (N);
+         T.Assert (Condition => Res = not Is_Neg,
+                   Message   => Test_Name & ".'<' returned invalid value: "
+                   & Boolean'Image (Res) & " when we expected: "
+                   & Boolean'Image (not Is_Neg)
+                   & " with value: " & V);
+         Res := To_Object_Test (N) + To_Object_Test (N) > To_Object_Test (N);
+         T.Assert (Condition => Res = not Is_Neg,
+                   Message   => Test_Name & ".'>' returned invalid value: "
+                   & Boolean'Image (Res) & " when we expected: "
+                   & Boolean'Image (not Is_Neg)
+                   & " with value: " & V);
+      end if;
+   end Test_Lt_Gt;
+
+   procedure Test_Lt_Gt is new Test_Basic_Object (Test_Lt_Gt);
+
+   --  ------------------------------
+   --  Test Util.Beans.Objects."="
+   --  ------------------------------
+   procedure Test_Eq (T : Test; V : String; N : Test_Type) is
+      Res   : Boolean;
+   begin
+      Res := To_Object_Test (N) = To_Object_Test (N);
+      T.Assert (Condition => Res,
+                Message   => Test_Name & ".'=' returned invalid value: "
+                & Boolean'Image (Res) & " when we expected: true");
+
+      Res := To_Object_Test (N) = To_Object ("Something" & V);
+      T.Assert (Condition => Res = False,
+                Message   => Test_Name & ".'=' returned invalid value: "
+                & Boolean'Image (Res) & " where we expected: False");
+   end Test_Eq;
+   procedure Test_Eq is new Test_Basic_Object (Test_Eq);
+
+   --  ------------------------------
+   --  Test Util.Beans.Objects."="
+   --  ------------------------------
+   procedure Test_Perf (T : Test; V : String; N : Test_Type) is
+      pragma Unreferenced (T, V);
+
+      use Ada.Calendar;
+
+      Start : Ada.Calendar.Time;
+      Value : constant Util.Beans.Objects.Object := To_Object_Test (N);
+      D     : Duration;
+   begin
+      Start := Ada.Calendar.Clock;
+      for I in 1 .. 1_000 loop
+         declare
+            V : Util.Beans.Objects.Object := Value;
+         begin
+            V := V + V;
+
+            pragma Unreferenced (V);
+         end;
+      end loop;
+      D := Ada.Calendar.Clock - Start;
+      Ada.Text_IO.Put_Line ("Perf " & Test_Name & ": " & Duration'Image (D * 1000.0));
+   end Test_Perf;
+   procedure Test_Perf is new Test_Basic_Object (Test_Perf);
+
+   package Caller is new AUnit.Test_Caller (Test);
+
+   procedure Add_Tests (Suite : Access_Test_Suite) is
+   begin
+      Suite.Add_Test (Caller.Create ("Test Util.Beans.Objects.To_Object." & Test_Name,
+                                     Test_To_Object'Access));
+      Suite.Add_Test (Caller.Create ("Test Util.Beans.Objects.To_String." & Test_Name,
+                                     Test_To_Object'Access));
+      Suite.Add_Test (Caller.Create ("Test Util.Beans.Objects.'='." & Test_Name,
+                                     Test_Eq'Access));
+      Suite.Add_Test (Caller.Create ("Test Util.Beans.Objects.'+'." & Test_Name,
+                                     Test_Add'Access));
+      Suite.Add_Test (Caller.Create ("Test Util.Beans.Objects.'-'." & Test_Name,
+                                     Test_Sub'Access));
+      Suite.Add_Test (Caller.Create ("Test Util.Beans.Objects.'<'." & Test_Name,
+                                     Test_Lt_Gt'Access));
+      Suite.Add_Test (Caller.Create ("Test Util.Beans.Objects.'>'." & Test_Name,
+                                     Test_Lt_Gt'Access));
+      Suite.Add_Test (Caller.Create ("Performance Util.Beans.Objects.'>'." & Test_Name,
+                                     Test_Perf'Access));
+      Suite.Add_Test (Caller.Create ("Test Util.Beans.Objects.Hash." & Test_Name,
+                                     Test_Hash'Access));
+   end Add_Tests;
+
+end Util.Beans.Objects.Discrete_Tests;
---
+++ regtests/util-beans-objects-discretes.adb
@@ -0,0 +1,172 @@
+-----------------------------------------------------------------------
+--  Util.Beans.Objects.Discretes -- Unit tests for concurrency package
+--  Copyright (C) 2009, 2010 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+with Ada.Calendar;
+with Ada.Calendar.Formatting;
+
+with Util.Beans.Objects;
+with Util.Beans.Objects.Enums;
+with Util.Beans.Objects.Time;
+with Util.Beans.Objects.Discrete_Tests;
+package body Util.Beans.Objects.Discretes is
+
+   use Ada.Calendar;
+
+   package Test_Integer is new
+     Util.Beans.Objects.Discrete_Tests (Test_Type      => Integer,
+                                        To_Type        => Util.Beans.Objects.To_Integer,
+                                        To_Object_Test => Util.Beans.Objects.To_Object,
+                                        Value          => Integer'Value,
+                                        Test_Name      => "Integer",
+                                        Test_Values    => "-100,1,0,1,1000");
+
+   package Test_Long_Integer is new
+     Util.Beans.Objects.Discrete_Tests (Test_Type      => Long_Integer,
+                                To_Type        => Util.Beans.Objects.To_Long_Integer,
+                                To_Object_Test => Util.Beans.Objects.To_Object,
+                                Value          => Long_Integer'Value,
+                                Test_Name      => "Long_Integer",
+                                Test_Values    => "-100,1,0,1,1000");
+
+   package Test_Long_Long_Integer is new
+     Util.Beans.Objects.Discrete_Tests (Test_Type      => Long_Long_Integer,
+                                To_Type        => Util.Beans.Objects.To_Long_Long_Integer,
+                                To_Object_Test => Util.Beans.Objects.To_Object,
+                                Value          => Long_Long_Integer'Value,
+                                Test_Name      => "Long_Long_Integer",
+                                Test_Values => "-10000000000000,1,0,1,1000_000_000_000");
+
+   function "-" (Left, Right : Boolean) return Boolean is
+   begin
+      return Left and Right;
+   end "-";
+
+   function "+" (Left, Right : Boolean) return Boolean is
+   begin
+      return Left or Right;
+   end "+";
+
+   package Test_Boolean is new
+     Util.Beans.Objects.Discrete_Tests (Test_Type      => Boolean,
+                                To_Type        => Util.Beans.Objects.To_Boolean,
+                                To_Object_Test => Util.Beans.Objects.To_Object,
+                                Value          => Boolean'Value,
+                                Test_Name      => "Boolean",
+                                Test_Values    => "false,true");
+
+   type Color is (WHITE, BLACK, RED, GREEN, BLUE, YELLOW);
+
+   package Color_Object is new Util.Beans.Objects.Enums (Color, ROUND_VALUE => True);
+
+   function "-" (Left, Right : Color) return Color is
+      N : constant Integer := Color'Pos (Left) - Color'Pos (Right);
+   begin
+      if N >= 0 then
+         return Color'Val ((Color'Pos (WHITE) + N) mod 6);
+      else
+         return Color'Val ((Color'Pos (WHITE) - N) mod 6);
+      end if;
+   end "-";
+
+   function "+" (Left, Right : Color) return Color is
+      N : constant Integer := Color'Pos (Left) + Color'Pos (Right);
+   begin
+      return Color'Val ((Color'Pos (WHITE) + N) mod 6);
+   end "+";
+
+   package Test_Enum is new
+     Util.Beans.Objects.Discrete_Tests (Test_Type      => Color,
+                                To_Type        => Color_Object.To_Value,
+                                To_Object_Test => Color_Object.To_Object,
+                                Value          => Color'Value,
+                                Test_Name      => "Color",
+                                Test_Values    => "BLACK,RED,GREEN,BLUE,YELLOW");
+
+   Epoch : constant Ada.Calendar.Time :=
+     Ada.Calendar.Time_Of (Year    => Year_Number'First,
+                           Month   => 1,
+                           Day     => 1,
+                           Seconds => 12 * 3600.0);
+
+   function Time_Value (S : String) return Ada.Calendar.Time is
+   begin
+      return Ada.Calendar.Formatting.Value (S);
+   end Time_Value;
+
+   --  For the purpose of the time unit test, we need Time + Time operation even
+   --  if this does not really makes sense.
+   function "+" (Left, Right : Ada.Calendar.Time) return Ada.Calendar.Time is
+      T1 : constant Duration := Left - Epoch;
+      T2 : constant Duration := Right - Epoch;
+   begin
+      return (T1 + T2) + Epoch;
+   end "+";
+
+   function "-" (Left, Right : Ada.Calendar.Time) return Ada.Calendar.Time is
+      T1 : constant Duration := Left - Epoch;
+      T2 : constant Duration := Right - Epoch;
+   begin
+      return (T1 - T2) + Epoch;
+   end "-";
+
+   package Test_Time is new
+     Util.Beans.Objects.Discrete_Tests (Test_Type      => Ada.Calendar.Time,
+                                To_Type        => Util.Beans.Objects.Time.To_Time,
+                                To_Object_Test => Util.Beans.Objects.Time.To_Object,
+                                Value          => Time_Value,
+                                Test_Name      => "Time",
+                                Test_Values => "1970-03-04 12:12:00,1975-05-04 13:13:10");
+
+   package Test_Float is new
+     Util.Beans.Objects.Discrete_Tests (Test_Type      => Float,
+                                To_Type        => Util.Beans.Objects.To_Float,
+                                To_Object_Test => Util.Beans.Objects.To_Object,
+                                Value          => Float'Value,
+                                Test_Name      => "Float",
+                                Test_Values    => "1.2,3.3,-3.3");
+
+   package Test_Long_Float is new
+     Util.Beans.Objects.Discrete_Tests (Test_Type      => Long_Float,
+                                To_Type        => Util.Beans.Objects.To_Long_Float,
+                                To_Object_Test => Util.Beans.Objects.To_Object,
+                                Value          => Long_Float'Value,
+                                Test_Name      => "Long_Float",
+                                Test_Values    => "1.2,3.3,-3.3");
+
+   package Test_Long_Long_Float is new
+     Util.Beans.Objects.Discrete_Tests (Test_Type      => Long_Long_Float,
+                                To_Type        => Util.Beans.Objects.To_Long_Long_Float,
+                                To_Object_Test => Util.Beans.Objects.To_Object,
+                                Value          => Long_Long_Float'Value,
+                                Test_Name      => "Long_Long_Float",
+                                Test_Values    => "1.2,3.3,-3.3");
+
+   procedure Add_Tests (Suite : AUnit.Test_Suites.Access_Test_Suite) is
+   begin
+      Test_Boolean.Add_Tests (Suite);
+      Test_Integer.Add_Tests (Suite);
+      Test_Long_Integer.Add_Tests (Suite);
+      Test_Long_Long_Integer.Add_Tests (Suite);
+      Test_Time.Add_Tests (Suite);
+      Test_Float.Add_Tests (Suite);
+      Test_Long_Float.Add_Tests (Suite);
+      Test_Long_Long_Float.Add_Tests (Suite);
+      Test_Enum.Add_Tests (Suite);
+   end Add_Tests;
+
+end Util.Beans.Objects.Discretes;
