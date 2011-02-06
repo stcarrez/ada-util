0c4e2f36f90ee4f2e32cbc9cd08ff1f970dc3b53
Stephane.Carrez <Stephane.Carrez@2d814c7b-9a8a-7a73-480f-6df5a50947db>
2011-02-06 20:00:48 +0000

    Simple stack for Util.Serialize.IO implementation

---
+++ src/util-stacks.adb
@@ -0,0 +1,76 @@
+-----------------------------------------------------------------------
+--  util-stacks -- Simple stack
+--  Copyright (C) 2010, 2011 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+with Ada.Unchecked_Deallocation;
+
+package body Util.Stacks is
+
+   procedure Free is
+     new Ada.Unchecked_Deallocation (Element_Type_Array,
+                                     Element_Type_Array_Access);
+
+   --  ------------------------------
+   --  Get access to the current stack element.
+   --  ------------------------------
+   function Current (Container : in Stack) return Element_Type_Access is
+   begin
+      return Container.Current;
+   end Current;
+
+   --  ------------------------------
+   --  Push an element on top of the stack making the new element the current one.
+   --  ------------------------------
+   procedure Push (Container : in out Stack) is
+   begin
+      if Container.Stack = null then
+         Container.Stack := new Element_Type_Array (1 .. 100);
+         Container.Pos := Container.Stack'First;
+      elsif Container.Pos = Container.Stack'Last then
+         declare
+            Old : Element_Type_Array_Access := Container.Stack;
+         begin
+            Container.Stack := new Element_Type_Array (1 .. Old'Last + 100);
+            Container.Stack (1 .. Old'Last) := Old (1 .. Old'Last);
+            Free (Old);
+         end;
+      end if;
+      if Container.Pos /= Container.Stack'First then
+         Container.Stack (Container.Pos + 1) := Container.Stack (Container.Pos);
+      end if;
+      Container.Pos := Container.Pos + 1;
+      Container.Current := Container.Stack (Container.Pos)'Access;
+   end Push;
+
+   --  ------------------------------
+   --  Pop the top element.
+   --  ------------------------------
+   procedure Pop (Container : in out Stack) is
+   begin
+      Container.Pos := Container.Pos - 1;
+      Container.Current   := Container.Stack (Container.Pos)'Access;
+   end Pop;
+
+   --  ------------------------------
+   --  Release the stack
+   --  ------------------------------
+   overriding
+   procedure Finalize (Obj : in out Stack) is
+   begin
+      Free (Obj.Stack);
+   end Finalize;
+
+end Util.Stacks;
---
+++ src/util-stacks.ads
@@ -0,0 +1,49 @@
+-----------------------------------------------------------------------
+--  util-stacks -- Simple stack
+--  Copyright (C) 2010, 2011 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+with Ada.Finalization;
+generic
+   type Element_Type is private;
+   type Element_Type_Access is access all Element_Type;
+package Util.Stacks is
+
+   type Stack is limited private;
+
+   --  Get access to the current stack element.
+   function Current (Container : in Stack) return Element_Type_Access;
+
+   --  Push an element on top of the stack making the new element the current one.
+   procedure Push (Container : in out Stack);
+
+   --  Pop the top element.
+   procedure Pop (Container : in out Stack);
+
+private
+   type Element_Type_Array is array (Natural range <>) of aliased Element_Type;
+   type Element_Type_Array_Access is access all Element_Type_Array;
+
+   type Stack is new Ada.Finalization.Limited_Controlled with record
+      Current : Element_Type_Access := null;
+      Stack   : Element_Type_Array_Access := null;
+      Pos     : Natural := 0;
+   end record;
+
+   --  Release the stack
+   overriding
+   procedure Finalize (Obj : in out Stack);
+
+end Util.Stacks;
