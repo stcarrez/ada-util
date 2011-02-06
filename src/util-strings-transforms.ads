343683e7edf22aa3e28922535241c3d5963ce38e
Stephane.Carrez <Stephane.Carrez@2d814c7b-9a8a-7a73-480f-6df5a50947db>
2011-02-06 22:50:52 +0000

    Add the To_Hex procedure

--- src/util-strings-transforms.ads
+++ src/util-strings-transforms.ads
@@ -55,6 +55,10 @@ package Util.Strings.Transforms is
    function To_Lower_Case (Content : String) return String
                            renames TR.To_Lower_Case;
 
+   --  Write in the output stream the value as a \uNNNN encoding form.
+   procedure To_Hex (Into  : in out Unbounded_String;
+                     Value : in Character) renames TR.To_Hex;
+
    --  Escape the content into the result stream using the JavaScript
    --  escape rules.
    procedure Escape_Javascript (Content : in String;
