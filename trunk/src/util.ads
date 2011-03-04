68f92f37f5a39460299c8ed7032c5cb9086eec44
Stephane.Carrez <Stephane.Carrez@2d814c7b-9a8a-7a73-480f-6df5a50947db>
2011-03-04 16:32:03 +0000

    Make sure we have an appender when reporting an error for the logger initialization Add the SVN identification in root package

--- trunk/src/util.ads
+++ trunk/src/util.ads
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  Util -- Various Utility Packages
---  Copyright (C) 2001, 2002, 2003, 2009, 2010 Stephane Carrez
+--  Copyright (C) 2001, 2002, 2003, 2009, 2010, 2011 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -18,5 +18,11 @@
 package Util is
 
    pragma Pure;
+
+   --  Library SVN identification
+   SVN_URL : constant String := "https://ada-util.googlecode.com/svn/trunk";
+
+   --  Revision used (must run 'make version' to update)
+   SVN_REV : constant Positive := 184;
 
 end Util;
