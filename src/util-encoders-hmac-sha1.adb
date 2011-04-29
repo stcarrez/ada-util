cb200c646ba63cad95d61efe18ae714d82f79ae1
Stephane.Carrez <Stephane.Carrez@2d814c7b-9a8a-7a73-480f-6df5a50947db>
2011-04-29 21:33:49 +0000

    Implement HMAC-SHA1 - RFC 2104, HMAC: Keyed-Hashing for Message Authentication

---
+++ src/util-encoders-hmac-sha1.adb
@@ -0,0 +1,205 @@
+-----------------------------------------------------------------------
+--  util-encoders-hmac-sha1 -- Compute HMAC-SHA1 authentication code
+--  Copyright (C) 2011 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+with Util.Encoders.Base16;
+
+--  The <b>Util.Encodes.HMAC.SHA1</b> package generates HMAC-SHA1 authentication
+--  (See RFC 2104 - HMAC: Keyed-Hashing for Message Authentication).
+package body Util.Encoders.HMAC.SHA1 is
+
+   --  ------------------------------
+   --  Sign the data string with the key and return the HMAC-SHA1 code in binary.
+   --  ------------------------------
+   function Sign (Key  : in String;
+                  Data : in String) return Util.Encoders.SHA1.Hash_Array is
+      Ctx    : Context;
+      Result : Util.Encoders.SHA1.Hash_Array;
+   begin
+      Set_Key (Ctx, Key);
+      Update (Ctx, Data);
+      Finish (Ctx, Result);
+      return Result;
+   end Sign;
+
+   --  ------------------------------
+   --  Sign the data string with the key and return the HMAC-SHA1 code as hexadecimal string.
+   --  ------------------------------
+   function Sign (Key  : in String;
+                  Data : in String) return Util.Encoders.SHA1.Digest is
+      Ctx    : Context;
+      Result : Util.Encoders.SHA1.Digest;
+   begin
+      Set_Key (Ctx, Key);
+      Update (Ctx, Data);
+      Finish (Ctx, Result);
+      return Result;
+   end Sign;
+
+   --  ------------------------------
+   --  Set the hmac private key.  The key must be set before calling any <b>Update</b>
+   --  procedure.
+   --  ------------------------------
+   procedure Set_Key (E   : in out Context;
+                      Key : in String) is
+      Buf : Ada.Streams.Stream_Element_Array (1 .. Key'Length);
+      for Buf'Address use Key'Address;
+      pragma Import (Ada, Buf);
+
+   begin
+      Set_Key (E, Buf);
+   end Set_Key;
+
+   IPAD : constant Ada.Streams.Stream_Element := 16#36#;
+   OPAD : constant Ada.Streams.Stream_Element := 16#5c#;
+
+   --  ------------------------------
+   --  Set the hmac private key.  The key must be set before calling any <b>Update</b>
+   --  procedure.
+   --  ------------------------------
+   procedure Set_Key (E   : in out Context;
+                      Key : in Ada.Streams.Stream_Element_Array) is
+      use type Ada.Streams.Stream_Element_Offset;
+      use type Ada.Streams.Stream_Element;
+   begin
+      --  Reduce the key
+      if Key'Length > 64 then
+         Util.Encoders.SHA1.Update (E.SHA, Key);
+         Util.Encoders.SHA1.Finish (E.SHA, E.Key (0 .. 19));
+         E.Key_Len := 19;
+      else
+         E.Key_Len := Key'Length - 1;
+         E.Key (0 .. E.Key_Len) := Key;
+      end if;
+
+      --  Hash the key in the SHA1 context.
+      declare
+         Block : Ada.Streams.Stream_Element_Array (0 .. 63);
+      begin
+         for I in 0 .. E.Key_Len loop
+            Block (I) := IPAD xor E.Key (I);
+         end loop;
+         for I in E.Key_Len + 1 .. 63 loop
+            Block (I) := IPAD;
+         end loop;
+         Util.Encoders.SHA1.Update (E.SHA, Block);
+      end;
+   end Set_Key;
+
+   --  ------------------------------
+   --  Update the hash with the string.
+   --  ------------------------------
+   procedure Update (E : in out Context;
+                     S : in String) is
+   begin
+      Util.Encoders.SHA1.Update (E.SHA, S);
+   end Update;
+
+   --  ------------------------------
+   --  Update the hash with the string.
+   --  ------------------------------
+   procedure Update (E : in out Context;
+                     S : in Ada.Streams.Stream_Element_Array) is
+   begin
+      Util.Encoders.SHA1.Update (E.SHA, S);
+   end Update;
+
+   --  ------------------------------
+   --  Computes the HMAC-SHA1 with the private key and the data collected by
+   --  the <b>Update</b> procedures.  Returns the raw binary hash in <b>Hash</b>.
+   --  ------------------------------
+   procedure Finish (E    : in out Context;
+                     Hash : out Util.Encoders.SHA1.Hash_Array) is
+      use type Ada.Streams.Stream_Element;
+      use type Ada.Streams.Stream_Element_Offset;
+   begin
+      Util.Encoders.SHA1.Finish (E.SHA, Hash);
+
+      --  Hash the key in the SHA1 context.
+      declare
+         Block : Ada.Streams.Stream_Element_Array (0 .. 63);
+      begin
+         for I in 0 .. E.Key_Len loop
+            Block (I) := OPAD xor E.Key (I);
+         end loop;
+         if E.Key_Len < 63 then
+            for I in E.Key_Len + 1 .. 63 loop
+               Block (I) := OPAD;
+            end loop;
+         end if;
+         Util.Encoders.SHA1.Update (E.SHA, Block);
+      end;
+      Util.Encoders.SHA1.Update (E.SHA, Hash);
+      Util.Encoders.SHA1.Finish (E.SHA, Hash);
+   end Finish;
+
+   --  ------------------------------
+   --  Computes the HMAC-SHA1 with the private key and the data collected by
+   --  the <b>Update</b> procedures.  Returns the hexadecimal hash in <b>Hash</b>.
+   --  ------------------------------
+   procedure Finish (E    : in out Context;
+                     Hash : out Util.Encoders.SHA1.Digest) is
+      Buf : Ada.Streams.Stream_Element_Array (1 .. Hash'Length);
+      for Buf'Address use Hash'Address;
+      pragma Import (Ada, Buf);
+
+      H       : Util.Encoders.SHA1.Hash_Array;
+      B       : Util.Encoders.Base16.Encoder;
+      Last    : Ada.Streams.Stream_Element_Offset;
+      Encoded : Ada.Streams.Stream_Element_Offset;
+   begin
+      Finish (E, H);
+      B.Transform (Data => H, Into => Buf, Last => Last, Encoded => Encoded);
+   end Finish;
+
+   --  Encodes the binary input stream represented by <b>Data</b> into
+   --  an SHA-1 hash output stream <b>Into</b>.
+   --
+   --  If the transformer does not have enough room to write the result,
+   --  it must return in <b>Encoded</b> the index of the last encoded
+   --  position in the <b>Data</b> stream.
+   --
+   --  The transformer returns in <b>Last</b> the last valid position
+   --  in the output stream <b>Into</b>.
+   --
+   --  The <b>Encoding_Error</b> exception is raised if the input
+   --  stream cannot be transformed.
+   overriding
+   procedure Transform (E       : in Encoder;
+                        Data    : in Ada.Streams.Stream_Element_Array;
+                        Into    : out Ada.Streams.Stream_Element_Array;
+                        Last    : out Ada.Streams.Stream_Element_Offset;
+                        Encoded : out Ada.Streams.Stream_Element_Offset) is
+   begin
+      null;
+   end Transform;
+
+   --  Delete the encoder object.
+   overriding
+   procedure Delete (E : access Encoder) is
+   begin
+      null;
+   end Delete;
+
+   --  Initialize the SHA-1 context.
+   overriding
+   procedure Initialize (E : in out Context) is
+   begin
+      null;
+   end Initialize;
+
+end Util.Encoders.HMAC.SHA1;
---
+++ src/util-encoders-hmac-sha1.ads
@@ -0,0 +1,113 @@
+-----------------------------------------------------------------------
+--  util-encoders-hmac-sha1 -- Compute HMAC-SHA1 authentication code
+--  Copyright (C) 2011 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+with Ada.Streams;
+with Ada.Finalization;
+with Util.Encoders.SHA1;
+
+--  The <b>Util.Encodes.HMAC.SHA1</b> package generates HMAC-SHA1 authentication
+--  (See RFC 2104 - HMAC: Keyed-Hashing for Message Authentication).
+package Util.Encoders.HMAC.SHA1 is
+
+   pragma Preelaborate;
+
+   --  Sign the data string with the key and return the HMAC-SHA1 code in binary.
+   function Sign (Key  : in String;
+                  Data : in String) return Util.Encoders.SHA1.Hash_Array;
+
+   --  Sign the data string with the key and return the HMAC-SHA1 code as hexadecimal string.
+   function Sign (Key  : in String;
+                  Data : in String) return Util.Encoders.SHA1.Digest;
+
+   --  ------------------------------
+   --  HMAC-SHA1 Context
+   --  ------------------------------
+   type Context is limited private;
+
+   --  Set the hmac private key.  The key must be set before calling any <b>Update</b>
+   --  procedure.
+   procedure Set_Key (E   : in out Context;
+                      Key : in String);
+
+   --  Set the hmac private key.  The key must be set before calling any <b>Update</b>
+   --  procedure.
+   procedure Set_Key (E   : in out Context;
+                      Key : in Ada.Streams.Stream_Element_Array);
+
+   --  Update the hash with the string.
+   procedure Update (E : in out Context;
+                     S : in String);
+
+   --  Update the hash with the string.
+   procedure Update (E : in out Context;
+                     S : in Ada.Streams.Stream_Element_Array);
+
+   --  Computes the HMAC-SHA1 with the private key and the data collected by
+   --  the <b>Update</b> procedures.  Returns the raw binary hash in <b>Hash</b>.
+   procedure Finish (E    : in out Context;
+                     Hash : out Util.Encoders.SHA1.Hash_Array);
+
+   --  Computes the HMAC-SHA1 with the private key and the data collected by
+   --  the <b>Update</b> procedures.  Returns the hexadecimal hash in <b>Hash</b>.
+   procedure Finish (E    : in out Context;
+                     Hash : out Util.Encoders.SHA1.Digest);
+
+   --  ------------------------------
+   --  HMAC-SHA1 encoder
+   --  ------------------------------
+   --  This <b>Encoder</b> translates the (binary) input stream into
+   --  an SHA1 hexadecimal stream.  The encoding alphabet is: 0123456789ABCDEF.
+   type Encoder is new Util.Encoders.Transformer with private;
+
+   --  Encodes the binary input stream represented by <b>Data</b> into
+   --  an SHA-1 hash output stream <b>Into</b>.
+   --
+   --  If the transformer does not have enough room to write the result,
+   --  it must return in <b>Encoded</b> the index of the last encoded
+   --  position in the <b>Data</b> stream.
+   --
+   --  The transformer returns in <b>Last</b> the last valid position
+   --  in the output stream <b>Into</b>.
+   --
+   --  The <b>Encoding_Error</b> exception is raised if the input
+   --  stream cannot be transformed.
+   overriding
+   procedure Transform (E       : in Encoder;
+                        Data    : in Ada.Streams.Stream_Element_Array;
+                        Into    : out Ada.Streams.Stream_Element_Array;
+                        Last    : out Ada.Streams.Stream_Element_Offset;
+                        Encoded : out Ada.Streams.Stream_Element_Offset);
+
+   --  Delete the encoder object.
+   overriding
+   procedure Delete (E : access Encoder);
+
+private
+
+   type Encoder is new Util.Encoders.Transformer with null record;
+
+   type Context is new Ada.Finalization.Limited_Controlled with record
+      SHA     : Util.Encoders.SHA1.Context;
+      Key     : Ada.Streams.Stream_Element_Array (0 .. 63);
+      Key_Len : Ada.Streams.Stream_Element_Offset;
+   end record;
+
+   --  Initialize the SHA-1 context.
+   overriding
+   procedure Initialize (E : in out Context);
+
+end Util.Encoders.HMAC.SHA1;
