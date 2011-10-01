8482eb3e327a2d5be2d2ecb2a628e4a349ab00b2
Stephane.Carrez <Stephane.Carrez@2d814c7b-9a8a-7a73-480f-6df5a50947db>
2011-10-01 21:53:44 +0000

    Example for date format

---
+++ samples/date.adb
@@ -0,0 +1,77 @@
+-----------------------------------------------------------------------
+--  date -- Print the date
+--  Copyright (C) 2011 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+with Ada.Text_IO;
+with Ada.Calendar;
+with Ada.Strings.Unbounded;
+with GNAT.Command_Line;
+
+with Util.Log.Loggers;
+with Util.Dates.Formats;
+with Util.Properties.Bundles;
+procedure Date is
+
+   use Util.Log.Loggers;
+   use Ada.Strings.Unbounded;
+   use Util.Properties.Bundles;
+   use GNAT.Command_Line;
+
+   Log     : constant Logger := Create ("log", "samples/log4j.properties");
+
+   Factory : Util.Properties.Bundles.Loader;
+
+   Bundle  : Util.Properties.Bundles.Manager;
+
+   Locale  : Unbounded_String := To_Unbounded_String ("en");
+   Result  : Ada.Strings.Unbounded.Unbounded_String;
+begin
+   --  Load the bundles from the current directory
+   Initialize (Factory, "samples/;bundles");
+   begin
+      Load_Bundle (Factory, "dates", To_String (Locale), Bundle);
+
+   exception
+      when NO_BUNDLE =>
+         Log.Error ("There is no bundle: {0}", "dates");
+   end;
+   loop
+      case Getopt ("h l: locale: help") is
+         when ASCII.NUL =>
+            exit;
+
+         when 'l' =>
+            Locale := To_Unbounded_String (Parameter);
+
+         when others =>
+            Log.Info ("Usage: date -l locale format");
+            return;
+      end case;
+   end loop;
+   loop
+      declare
+         Pattern : constant String := Get_Argument;
+      begin
+         exit when Pattern = "";
+
+         Util.Dates.Formats.Format (Pattern => Pattern,
+                                    Date    => Ada.Calendar.Clock,
+                                    Bundle  => Bundle,
+                                    Into    => Result);
+         Ada.Text_IO.Put_Line (Ada.Strings.Unbounded.To_String (Result));
+      end;
+   end loop;
+end Date;
