946020b21ec2134821d000e1a8a42f4eaec72d94
Stephane.Carrez <Stephane.Carrez@2d814c7b-9a8a-7a73-480f-6df5a50947db>
2011-12-06 20:27:04 +0000

    Fix using the name when registering a test class

--- testutil/aunit/util-test_caller.adb
+++ testutil/aunit/util-test_caller.adb
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  AUnit utils - Helper for writing unit tests
---  Copyright (C) 2009, 2010 Stephane Carrez
+--  Copyright (C) 2009, 2010, 2011 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -18,11 +18,11 @@
 
 package body Util.Test_Caller is
 
-   procedure Add_Test (Suite  : in Util.Tests.Access_Test_Suite;
-                       Name   : in String;
-                       Method : in Caller.Test_Method) is
+   procedure Add_Test (Suite     : in Util.Tests.Access_Test_Suite;
+                       Test_Name : in String;
+                       Method    : in Caller.Test_Method) is
    begin
-      Suite.Add_Test (Caller.Create (Name, Method));
+      Suite.Add_Test (Caller.Create (Test_Name, Method));
    end Add_Test;
 
 end Util.Test_Caller;
