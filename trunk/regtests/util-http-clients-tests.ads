76e50bfd5d674fac69a48dbb1e27994e55a62d4f
Stephane.Carrez <Stephane.Carrez@2d814c7b-9a8a-7a73-480f-6df5a50947db>
2012-05-28 09:50:00 +0000

    Setup a generic package that can be instantiated to build a set of tests that use a specific HTTP implementation

--- trunk/regtests/util-http-clients-tests.ads
+++ trunk/regtests/util-http-clients-tests.ads
@@ -20,14 +20,25 @@ with Util.Tests;
 
 package Util.Http.Clients.Tests is
 
-   procedure Add_Tests (Suite : in Util.Tests.Access_Test_Suite);
-
    type Test is new Util.Tests.Test with null record;
-
-   overriding
-   procedure Set_Up (T : in out Test);
 
    --  Test the http Get operation.
    procedure Test_Http_Get (T : in out Test);
+
+   --  The <b>Http_Tests</b> package must be instantiated with one of the HTTP implementation.
+   --  The <b>Register</b> procedure configures the Http.Client to use the given HTTP
+   --  implementation before running the test.
+   generic
+      with procedure Register;
+      NAME : in String;
+   package Http_Tests is
+      procedure Add_Tests (Suite : in Util.Tests.Access_Test_Suite);
+
+      type Http_Test is new Test with null record;
+
+      overriding
+      procedure Set_Up (T : in out Http_Test);
+
+   end Http_Tests;
 
 end Util.Http.Clients.Tests;
