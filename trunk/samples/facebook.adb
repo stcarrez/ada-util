d296bdeda58d5927ec623365f9729d43de6c581e
Stephane.Carrez <Stephane.Carrez@2d814c7b-9a8a-7a73-480f-6df5a50947db>
2012-05-30 18:50:09 +0000

    New example 'facebook' to use the Facebook public Graph API and retrieve a user information in an Ada record and print it

---
+++ trunk/samples/facebook.adb
@@ -0,0 +1,78 @@
+-----------------------------------------------------------------------
+--  facebook -- Get information about a Facebook user using the Facebook API
+--  Copyright (C) 2012 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+with Ada.Text_IO;
+with Ada.Command_Line;
+with Ada.Strings.Unbounded;
+
+with Util.Strings;
+with Util.Beans.Objects;
+with Util.Http.Clients;
+with Util.Http.Clients.Web;
+with Util.Serialize.IO.JSON;
+
+with Mapping;
+
+--  This example shows how to invoke a REST service, retrieve and extract a JSON content
+--  into some Ada record.  It uses the Facebook Graph API which does not need any
+--  authentication (ie, the public Facebook API).
+procedure Facebook is
+
+   procedure Print (P : in Mapping.Person) is
+      use Ada.Strings.Unbounded;
+   begin
+      Ada.Text_IO.Put_Line ("Id         : " & Long_Long_Integer'Image (P.Id));
+      Ada.Text_IO.Put_Line ("Name       : " & To_String (P.Name));
+      Ada.Text_IO.Put_Line ("First name : " & To_String (P.First_Name));
+      Ada.Text_IO.Put_Line ("Last name  : " & To_String (P.Last_Name));
+      Ada.Text_IO.Put_Line ("Username   : " & To_String (P.Username));
+      Ada.Text_IO.Put_Line ("Gender     : " & To_String (P.Gender));
+      Ada.Text_IO.Put_Line ("Link       : " & To_String (P.Link));
+   end Print;
+
+   Count : constant Natural := Ada.Command_Line.Argument_Count;
+
+   --  Mapping for the Person record.
+   Person_Mapping  : aliased Mapping.Person_Mapper.Mapper;
+begin
+   if Count = 0 then
+      Ada.Text_IO.Put_Line ("Usage: facebook username ...");
+      Ada.Text_IO.Put_Line ("Example: facebook btaylor");
+      return;
+   end if;
+   Person_Mapping.Add_Mapping ("id", Mapping.FIELD_ID);
+   Person_Mapping.Add_Mapping ("name", Mapping.FIELD_NAME);
+   Person_Mapping.Add_Mapping ("first_name", Mapping.FIELD_FIRST_NAME);
+   Person_Mapping.Add_Mapping ("last_name", Mapping.FIELD_LAST_NAME);
+   Person_Mapping.Add_Mapping ("link", Mapping.FIELD_LINK);
+   Person_Mapping.Add_Mapping ("username", Mapping.FIELD_USER_NAME);
+   Person_Mapping.Add_Mapping ("gender", Mapping.FIELD_GENDER);
+   Util.Http.Clients.Web.Register;
+   for I in 1 .. Count loop
+      declare
+         URI      : constant String := Ada.Command_Line.Argument (I);
+         P        : aliased Mapping.Person;
+	 
+	 procedure Get_User is
+	    new Mapping.Rest_Get (Mapping.Person, Mapping.Person_Access,
+				  Mapping.Person_Mapper.Set_Context);
+      begin
+	 Get_User ("https://graph.facebook.com/" & URI, Person_Mapping'Unchecked_Access, P'Unchecked_Access);
+	 Print (P);
+      end;
+   end loop;
+end Facebook;
