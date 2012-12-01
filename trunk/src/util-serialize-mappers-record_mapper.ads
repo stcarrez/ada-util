1eb83f5823c9779795a21fd31bbf0f521f0ec61b
Stephane.Carrez <Stephane.Carrez@2d814c7b-9a8a-7a73-480f-6df5a50947db>
2012-12-01 20:16:56 +0000

    Fix header

--- trunk/src/util-serialize-mappers-record_mapper.ads
+++ trunk/src/util-serialize-mappers-record_mapper.ads
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  Util.Serialize.Mappers.Record_Mapper -- Mapper for record types
---  Copyright (C) 2010, 2011 Stephane Carrez
+--  Copyright (C) 2010, 2011, 2012 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
