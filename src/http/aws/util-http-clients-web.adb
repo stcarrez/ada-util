e5668ec3a7199889aa6d90433a690fac49750801
Stephane.Carrez <Stephane.Carrez@2d814c7b-9a8a-7a73-480f-6df5a50947db>
2012-12-17 21:17:35 +0000

    Fix comment header

--- src/http/aws/util-http-clients-web.adb
+++ src/http/aws/util-http-clients-web.adb
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
---  asf-clients-web -- HTTP Clients with AWS implementation
---  Copyright (C) 2011 Stephane Carrez
+--  util-http-clients-web -- HTTP Clients with AWS implementation
+--  Copyright (C) 2011, 2012 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
