6eaad8116583c2287af8617b4bfba2f48f832702
Stephane.Carrez <Stephane.Carrez@2d814c7b-9a8a-7a73-480f-6df5a50947db>
2013-01-17 22:33:29 +0000

    Update the version

--- src/util.ads
+++ src/util.ads
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  Util -- Various Utility Packages
---  Copyright (C) 2001, 2002, 2003, 2009, 2010, 2011 Stephane Carrez
+--  Copyright (C) 2001, 2002, 2003, 2009, 2010, 2011, 2012, 2013 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -23,6 +23,6 @@ package Util is
    SVN_URL : constant String := "https://ada-util.googlecode.com/svn/trunk";
 
    --  Revision used (must run 'make version' to update)
-   SVN_REV : constant Positive := 259;
+   SVN_REV : constant Positive := 723;
 
 end Util;
