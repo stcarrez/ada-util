41119cc79740c5cbe899e97fa15bbc5e4e971356
Stephane.Carrez <Stephane.Carrez@2d814c7b-9a8a-7a73-480f-6df5a50947db>
2013-03-18 20:51:40 +0000

    Add unit tests for Util.Texts.Builders package Add performance CSV generation with various block sizes and a comparison with Unbounded_String

---
+++ regtests/util-texts-builders_tests.ads
@@ -0,0 +1,42 @@
+-----------------------------------------------------------------------
+--  util-texts-builders_tests -- Unit tests for text builders
+--  Copyright (C) 2013 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+with Util.Tests;
+
+package Util.Texts.Builders_Tests is
+
+   procedure Add_Tests (Suite : in Util.Tests.Access_Test_Suite);
+
+   type Test is new Util.Tests.Test with null record;
+
+   --  Test the length operation.
+   procedure Test_Length (T : in out Test);
+
+   --  Test the append operation.
+   procedure Test_Append (T : in out Test);
+
+   --  Test the iterate operation.
+   procedure Test_Iterate (T : in out Test);
+
+   --  Test the clear operation.
+   procedure Test_Clear (T : in out Test);
+
+   --  Test the append and iterate performance.
+   procedure Test_Perf (T : in out Test);
+
+end Util.Texts.Builders_Tests;
