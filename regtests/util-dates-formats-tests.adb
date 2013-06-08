a097af089d0208ab01cf9ac8ee310d0dbeecb88f
Stephane.Carrez <Stephane.Carrez@2d814c7b-9a8a-7a73-480f-6df5a50947db>
2013-06-08 15:05:38 +0000

    Add unit tests for Get_Day_Start, Get_Week_Start and Get_Month_Start

--- regtests/util-dates-formats-tests.adb
+++ regtests/util-dates-formats-tests.adb
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  util-dates-formats-tests - Test for date formats
---  Copyright (C) 2011 Stephane Carrez
+--  Copyright (C) 2011, 2013 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -19,16 +19,26 @@
 with Ada.Calendar.Formatting;
 with Util.Test_Caller;
 with Util.Properties.Bundles;
+with Util.Log.Loggers;
 package body Util.Dates.Formats.Tests is
    use Util.Tests;
    use Ada.Strings.Unbounded;
 
-   package Caller is new Util.Test_Caller (Test, "Dates.Formats");
+   --  The logger
+   Log : constant Util.Log.Loggers.Logger := Util.Log.Loggers.Create ("Util.Dates.Formats.Tests");
+
+   package Caller is new Util.Test_Caller (Test, "Dates");
 
    procedure Add_Tests (Suite : in Util.Tests.Access_Test_Suite) is
    begin
       Caller.Add_Test (Suite, "Test Util.Dates.Formats.Format",
                        Test_Format'Access);
+      Caller.Add_Test (Suite, "Test Util.Dates.Get_Day_Start",
+                       Test_Get_Day_Start'Access);
+      Caller.Add_Test (Suite, "Test Util.Dates.Get_Week_Start",
+                       Test_Get_Week_Start'Access);
+      Caller.Add_Test (Suite, "Test Util.Dates.Get_Month_Start",
+                       Test_Get_Month_Start'Access);
    end Add_Tests;
 
    procedure Test_Format (T : in out Test) is
@@ -74,5 +84,77 @@ package body Util.Dates.Formats.Tests is
       Check ("%g", T1, "80W01");
 
    end Test_Format;
+
+   procedure Check (T          : in out Test'Class;
+                    Year       : in Ada.Calendar.Year_Number;
+                    Month      : in Ada.Calendar.Month_Number;
+                    Day        : in Ada.Calendar.Day_Number;
+                    Expect_Day : in Ada.Calendar.Day_Number;
+                    Message    : in String;
+                    Operation  : access function (D : in Ada.Calendar.Time)
+                    return Ada.Calendar.Time) is
+      use type Ada.Calendar.Time;
+
+      Date : Ada.Calendar.Time := Ada.Calendar.Formatting.Time_Of (Year, Month, Day,
+                                                                   0, 0, 0);
+   begin
+      for I in 1 .. 47 loop
+         declare
+            R : constant Ada.Calendar.Time := Operation (Date);
+            D : Date_Record;
+         begin
+            Split (D, R);
+            Log.Info ("{0} ({1}) => {2}",
+                      Ada.Calendar.Formatting.Image (Date),
+                      Message, Ada.Calendar.Formatting.Image (R));
+            Util.Tests.Assert_Equals (T, Natural (Year), Natural (D.Year),
+                                      "Invalid year " & Message);
+            Util.Tests.Assert_Equals (T, Natural (Month), Natural (D.Month),
+                                      "Invalid month " & Message);
+            Util.Tests.Assert_Equals (T, Natural (Expect_Day), Natural (D.Month_Day),
+                                      "Invalid day " & Message);
+            Util.Tests.Assert_Equals (T, 0, Natural (D.Hour),
+                                      "Invalid hour " & Message);
+            Util.Tests.Assert_Equals (T, 0, Natural (D.Minute),
+                                      "Invalid minute " & Message);
+            Util.Tests.Assert_Equals (T, 0, Natural (D.Second),
+                                      "Invalid second " & Message);
+         end;
+         Date := Date + 1800.0;
+      end loop;
+   end Check;
+
+   --  ------------------------------
+   --  Test the Get_Day_Start operation.
+   --  ------------------------------
+   procedure Test_Get_Day_Start (T : in out Test) is
+   begin
+      Check (T, 2013, 6, 04, 04, "Get_Day_Start", Get_Day_Start'Access);
+      Check (T, 2010, 2, 14, 14, "Get_Day_Start", Get_Day_Start'Access);
+   end Test_Get_Day_Start;
+
+   --  ------------------------------
+   --  Test the Get_Week_Start operation.
+   --  ------------------------------
+   procedure Test_Get_Week_Start (T : in out Test) is
+   begin
+      Check (T, 2013, 6, 04, 03, "Get_Week_Start", Get_Week_Start'Access);
+      Check (T, 2013, 6, 03, 03, "Get_Week_Start", Get_Week_Start'Access);
+      Check (T, 2013, 6, 05, 03, "Get_Week_Start", Get_Week_Start'Access);
+      Check (T, 2013, 6, 08, 03, "Get_Week_Start", Get_Week_Start'Access);
+      Check (T, 2010, 2, 14, 08, "Get_Week_Start", Get_Week_Start'Access);
+      Check (T, 2010, 2, 13, 08, "Get_Week_Start", Get_Week_Start'Access);
+      Check (T, 2010, 2, 10, 08, "Get_Week_Start", Get_Week_Start'Access);
+      Check (T, 2010, 2, 15, 15, "Get_Week_Start", Get_Week_Start'Access);
+   end Test_Get_Week_Start;
+
+   --  ------------------------------
+   --  Test the Get_Month_Start operation.
+   --  ------------------------------
+   procedure Test_Get_Month_Start (T : in out Test) is
+   begin
+      Check (T, 2013, 6, 04, 01, "Get_Month_Start", Get_Month_Start'Access);
+      Check (T, 2010, 2, 14, 01, "Get_Month_Start", Get_Month_Start'Access);
+   end Test_Get_Month_Start;
 
 end Util.Dates.Formats.Tests;
