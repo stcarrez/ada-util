4be0d8e4fdaa41dae68ea370ae1b2512c7b02073
Stephane.Carrez <Stephane.Carrez@2d814c7b-9a8a-7a73-480f-6df5a50947db>
2013-06-08 15:04:40 +0000

    Implement the new functions Get_Day_Start, Get_Week_Start and Get_Month_Start

--- src/util-dates.adb
+++ src/util-dates.adb
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  util-dates -- Date utilities
---  Copyright (C) 2011 Stephane Carrez
+--  Copyright (C) 2011, 2013 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,7 +15,7 @@
 --  See the License for the specific language governing permissions and
 --  limitations under the License.
 -----------------------------------------------------------------------
-
+with Ada.Calendar.Arithmetic;
 package body Util.Dates is
 
    --  ------------------------------
@@ -39,5 +39,80 @@ package body Util.Dates is
                                      Time_Zone   => Time_Zone);
       Into.Day := Ada.Calendar.Formatting.Day_Of_Week (Date);
    end Split;
+
+   --  ------------------------------
+   --  Get a time representing the given date at 00:00:00.
+   --  ------------------------------
+   function Get_Day_Start (Date : in Date_Record) return Ada.Calendar.Time is
+   begin
+      return Ada.Calendar.Formatting.Time_Of (Year        => Date.Year,
+                                              Month       => Date.Month,
+                                              Day         => Date.Month_Day,
+                                              Hour        => 0,
+                                              Minute      => 0,
+                                              Second      => 0,
+                                              Sub_Second  => 0.0,
+                                              Time_Zone   => Date.Time_Zone);
+   end Get_Day_Start;
+
+   function Get_Day_Start (Date : in Ada.Calendar.Time) return Ada.Calendar.Time is
+      D : Date_Record;
+   begin
+      Split (D, Date);
+      return Get_Day_Start (D);
+   end Get_Day_Start;
+
+   --  ------------------------------
+   --  Get a time representing the beginning of the week at 00:00:00.
+   --  ------------------------------
+   function Get_Week_Start (Date : in Date_Record) return Ada.Calendar.Time is
+      use Ada.Calendar.Formatting;
+      use Ada.Calendar.Arithmetic;
+
+      T : constant Ada.Calendar.Time := Time_Of (Year        => Date.Year,
+                                                 Month       => Date.Month,
+                                                 Day         => Date.Month_Day,
+                                                 Hour        => 0,
+                                                 Minute      => 0,
+                                                 Second      => 0,
+                                                 Sub_Second  => 0.0,
+                                                 Time_Zone   => Date.Time_Zone);
+      Day : constant Ada.Calendar.Formatting.Day_Name := Ada.Calendar.Formatting.Day_Of_Week (T);
+   begin
+      if Day = Ada.Calendar.Formatting.Monday then
+         return T;
+      else
+         return T - Day_Count (Day_Name'Pos (Day) - Day_Name'Pos (Monday));
+      end if;
+   end Get_Week_Start;
+
+   function Get_Week_Start (Date : in Ada.Calendar.Time) return Ada.Calendar.Time is
+      D : Date_Record;
+   begin
+      Split (D, Date);
+      return Get_Week_Start (D);
+   end Get_Week_Start;
+
+   --  ------------------------------
+   --  Get a time representing the beginning of the month at 00:00:00.
+   --  ------------------------------
+   function Get_Month_Start (Date : in Date_Record) return Ada.Calendar.Time is
+   begin
+      return Ada.Calendar.Formatting.Time_Of (Year        => Date.Year,
+                                              Month       => Date.Month,
+                                              Day         => Ada.Calendar.Day_Number'First,
+                                              Hour        => 0,
+                                              Minute      => 0,
+                                              Second      => 0,
+                                              Sub_Second  => 0.0,
+                                              Time_Zone   => Date.Time_Zone);
+   end Get_Month_Start;
+
+   function Get_Month_Start (Date : in Ada.Calendar.Time) return Ada.Calendar.Time is
+      D : Date_Record;
+   begin
+      Split (D, Date);
+      return Get_Month_Start (D);
+   end Get_Month_Start;
 
 end Util.Dates;
