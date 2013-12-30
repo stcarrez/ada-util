431a69445d5df453592ce8e4e280c416a089ffdc
Stephane.Carrez <Stephane.Carrez@2d814c7b-9a8a-7a73-480f-6df5a50947db>
2013-12-30 22:09:19 +0000

    Use the Is_Test_Enabled function to ignore or take into account the test when it is added in the testsuite.

--- trunk/testutil/aunit/util-test_caller.adb
+++ trunk/testutil/aunit/util-test_caller.adb
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  AUnit utils - Helper for writing unit tests
---  Copyright (C) 2009, 2010, 2011 Stephane Carrez
+--  Copyright (C) 2009, 2010, 2011, 2013 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -22,7 +22,9 @@ package body Util.Test_Caller is
                        Test_Name : in String;
                        Method    : in Caller.Test_Method) is
    begin
-      Suite.Add_Test (Caller.Create (Test_Name, Method));
+      if Util.Tests.Is_Test_Enabled (Test_Name) then
+         Suite.Add_Test (Caller.Create (Test_Name, Method));
+      end if;
    end Add_Test;
 
 end Util.Test_Caller;
