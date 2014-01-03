3ca8cc74a2e76e70c71728ffa8390bbfbe060cd1
Stephane.Carrez <Stephane.Carrez@2d814c7b-9a8a-7a73-480f-6df5a50947db>
2014-01-03 22:45:43 +0000

    Add the source and line in the assertion message

--- testutil/ahven/util-assertions.adb
+++ testutil/ahven/util-assertions.adb
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  AUnit utils - Helper for writing unit tests
---  Copyright (C) 2009, 2010 Stephane Carrez
+--  Copyright (C) 2009, 2010, 2014 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -17,6 +17,7 @@
 -----------------------------------------------------------------------
 
 with Ahven;
+with Util.XUnit;
 package body Util.Assertions is
 
    --  ------------------------------
@@ -28,12 +29,16 @@ package body Util.Assertions is
                               Message : in String := "Test failed";
                               Source  : in String := GNAT.Source_Info.File;
                               Line    : in Natural := GNAT.Source_Info.Line) is
+      pragma Unreferenced (T);
    begin
       Ahven.Assert (Condition => Expect = Value,
-                               Message   => Message & ": expecting '"
-                               & Value_Type'Image (Expect) & "'"
-                               & " value was '"
-                               & Value_Type'Image (Value) & "'");
+                    Message   => Util.XUnit.Build_Message
+                      (Message => Message & ": expecting '"
+                       & Value_Type'Image (Expect) & "'"
+                       & " value was '"
+                       & Value_Type'Image (Value) & "'",
+                       Source => Source,
+                       Line => Line));
    end Assert_Equals_T;
 
 end Util.Assertions;
