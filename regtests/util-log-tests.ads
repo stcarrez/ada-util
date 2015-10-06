ebd8ddf9d549713a85697da45edf8344a27d3db2
ciceron <Stephane.Carrez@gmail.com>
2015-10-06 23:13:09 +0200

    Declare the Test_File_Appender_Modes procedure

--- regtests/util-log-tests.ads
+++ regtests/util-log-tests.ads
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  log.tests -- Unit tests for loggers
---  Copyright (C) 2009, 2010, 2011 Stephane Carrez
+--  Copyright (C) 2009, 2010, 2011, 2015 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -28,5 +28,8 @@ package Util.Log.Tests is
    procedure Test_Log (T : in out Test);
    procedure Test_File_Appender (T : in out Test);
    procedure Test_List_Appender (T : in out Test);
+
+   --  Test file appender with different modes.
+   procedure Test_File_Appender_Modes (T : in out Test);
 
 end Util.Log.Tests;
