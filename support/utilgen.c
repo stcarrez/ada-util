46d43398b8880ec7224b4522935f635efa362e16
Stephane Carrez <Stephane.Carrez@gmail.com>
2015-11-23 21:13:29 +0100

    Integrate FreeBSD/DragonFly patch from John Marino https://svnweb.freebsd.org/ports/head/devel/ada-util/files/patch-support_utilgen.c?revision=402249&view=co

--- support/utilgen.c
+++ support/utilgen.c
@@ -18,6 +18,7 @@
 # define _LARGEFILE64_SOURCE
 #endif
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/types.h>
@@ -178,6 +179,39 @@ void gen_stat(void)
     printf("      pad6    : Interfaces.C.unsigned_long;\n");
     printf("      pad7    : Interfaces.C.unsigned_long;\n");
 #endif
+    printf("   end record;\n");
+    printf("   pragma Convention (C_Pass_By_Copy, Stat_Type);\n");
+    printf("\n");
+#elif defined(__DragonFly__)
+    struct stat st;
+
+    gen_type("uint16_t", UNSIGNED, sizeof(uint16_t));
+    gen_type("u_int32_t", UNSIGNED, sizeof(u_int32_t));
+    gen_type("int32_t", SIGNED, sizeof(int32_t));
+    gen_type("int64_t", SIGNED, sizeof(int64_t));
+
+    printf("   STAT_NAME  : constant String := \"stat\";\n");
+    printf("   FSTAT_NAME : constant String := \"fstat\";\n");
+    printf("   type Stat_Type is record\n");
+    printf("      st_ino      : ino_t;\n");
+    printf("      st_nlink    : nlink_t;\n");
+    printf("      st_dev      : dev_t;\n");
+    printf("      st_mode     : mode_t;\n");
+    printf("      st_padding1 : uint16_t;\n");
+    printf("      st_uid      : uid_t;\n");
+    printf("      st_gid      : gid_t;\n");
+    printf("      st_rdev     : dev_t;\n");
+    printf("      st_atim     : Timespec;\n");
+    printf("      st_mtim     : Timespec;\n");
+    printf("      st_ctim     : Timespec;\n");
+    printf("      st_size     : off_t;\n");
+    printf("      st_blocks   : int64_t;\n");
+    printf("      st_blksize  : u_int32_t;\n");
+    printf("      st_flags    : u_int32_t;\n");
+    printf("      st_gen      : u_int32_t;\n");
+    printf("      st_lspare   : int32_t;\n");
+    printf("      st_qspare1  : int64_t;\n");
+    printf("      st_qspare2  : int64_t;\n");
     printf("   end record;\n");
     printf("   pragma Convention (C_Pass_By_Copy, Stat_Type);\n");
     printf("\n");
