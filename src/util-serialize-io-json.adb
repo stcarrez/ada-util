dac8931727db10787f8074fc91d9a26e0f0e2428
Stephane Carrez <Stephane.Carrez@gmail.com>
2016-08-20 20:42:19 +0200

    Implement the Write_Entity operation with a date/time

--- src/util-serialize-io-json.adb
+++ src/util-serialize-io-json.adb
@@ -25,6 +25,7 @@ with Ada.IO_Exceptions;
 with Util.Strings;
 with Util.Streams;
 with Util.Streams.Buffered;
+with Util.Dates.ISO8601;
 package body Util.Serialize.IO.JSON is
 
    use Ada.Strings.Unbounded;
@@ -333,6 +334,14 @@ package body Util.Serialize.IO.JSON is
                            Value  : in Integer) is
    begin
       Stream.Write_Attribute (Name, Value);
+   end Write_Entity;
+
+   overriding
+   procedure Write_Entity (Stream : in out Output_Stream;
+                           Name   : in String;
+                           Value  : in Ada.Calendar.Time) is
+   begin
+      Stream.Write_Entity (Name, Util.Dates.ISO8601.Image (Value, Util.Dates.ISO8601.SUBSECOND));
    end Write_Entity;
 
    overriding
