7b8384a3ab3cf0cbe1016e2f7cbf4d1a64a9a4a8
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-04-08 18:24:06 +0200

    Implement the Initialize and other operations for the String_Argument_List type

--- src/util-commands.adb
+++ src/util-commands.adb
@@ -16,6 +16,7 @@
 --  limitations under the License.
 -----------------------------------------------------------------------
 with Ada.Command_Line;
+with Ada.Characters.Handling;
 package body Util.Commands is
 
    --  ------------------------------
@@ -46,8 +47,66 @@ package body Util.Commands is
    --  Get the command name.
    --  ------------------------------
    function Get_Command_Name (List : in Default_Argument_List) return String is
+      pragma Unreferenced (List);
    begin
       return Ada.Command_Line.Command_Name;
+   end Get_Command_Name;
+
+   --  ------------------------------
+   --  Set the argument list to the given string and split the arguments.
+   --  ------------------------------
+   procedure Initialize (List : in out String_Argument_List;
+                         Line : in String) is
+      First : Natural := Line'First;
+   begin
+      List.Length := Line'Length;
+      List.Line (1 .. Line'Length) := Line;
+      List.Count := 0;
+      loop
+         while First <= Line'Length
+           and then Ada.Characters.Handling.Is_Space (List.Line (First)) loop
+            First := First + 1;
+         end loop;
+         exit when First > Line'Length;
+         List.Start_Pos (List.Count) := First;
+         while First <= Line'Length
+           and then not Ada.Characters.Handling.Is_Space (List.Line (First)) loop
+            First := First + 1;
+         end loop;
+         List.End_Pos (List.Count) := First - 1;
+         List.Count := List.Count + 1;
+      end loop;
+      if List.Count > 0 then
+         List.Count := List.Count - 1;
+      end if;
+   end Initialize;
+
+   --  ------------------------------
+   --  Get the number of arguments available.
+   --  ------------------------------
+   overriding
+   function Get_Count (List : in String_Argument_List) return Natural is
+   begin
+      return List.Count;
+   end Get_Count;
+
+   --  ------------------------------
+   --  Get the argument at the given position.
+   --  ------------------------------
+   overriding
+   function Get_Argument (List : in String_Argument_List;
+                          Pos  : in Positive) return String is
+   begin
+      return List.Line (List.Start_Pos (Pos) .. List.End_Pos (Pos));
+   end Get_Argument;
+
+   --  ------------------------------
+   --  Get the command name.
+   --  ------------------------------
+   overriding
+   function Get_Command_Name (List : in String_Argument_List) return String is
+   begin
+      return List.Line (List.Start_Pos (0) .. List.End_Pos (0));
    end Get_Command_Name;
 
 end Util.Commands;
