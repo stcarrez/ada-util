ddc765f9439e13d44116dfdeddc4df41e2336a17
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-04-11 09:21:00 +0200

    Simple coverage script

---
+++ coverage.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+lcov --base-directory . --directory . -c -o util.cov
+bin/util_harness -xml util-aunit.xml
+lcov --base-directory . --directory . -c -o util.cov
+lcov --remove util.cov "/usr*" -o util.cov
+lcov --remove util.cov "regtests*" -o util.cov
+lcov --remove util.cov ada-util/b__util_harness.adb -o util.cov
+lcov --remove util.cov ada-util/b__util_test_process.adb -o util.cov
+rm -rf cover
+genhtml -o ./cover -t "test coverage" --num-spaces 4 util.cov
+ 
