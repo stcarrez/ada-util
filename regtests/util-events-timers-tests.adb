3d4157d9959e902e7477076b7b08a493e2bc8347
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-08-01 22:04:57 +0200

    Implement the Test_Repeat_Timer procedure and register the new test

--- regtests/util-events-timers-tests.adb
+++ regtests/util-events-timers-tests.adb
@@ -31,6 +31,8 @@ package body Util.Events.Timers.Tests is
                        Test_Empty_Timer'Access);
       Caller.Add_Test (Suite, "Test Util.Events.Timers.Process",
                        Test_Timer_Event'Access);
+      Caller.Add_Test (Suite, "Test Util.Events.Timers.Repeat",
+                       Test_Repeat_Timer'Access);
    end Add_Tests;
 
    overriding
@@ -38,6 +40,10 @@ package body Util.Events.Timers.Tests is
                            Event : in out Timer_Ref'Class) is
    begin
       Sub.Count := Sub.Count + 1;
+      if Sub.Repeat > 1 then
+         Sub.Repeat := Sub.Repeat - 1;
+         Event.Repeat (Ada.Real_Time.Milliseconds (1));
+      end if;
    end Time_Handler;
 
    --  -----------------------
@@ -74,5 +80,32 @@ package body Util.Events.Timers.Tests is
       M.Process (Deadline);
       Assert_Equals (T, 1, T.Count, "The timer handler was not called");
    end Test_Timer_Event;
+
+   --  -----------------------
+   --  Test repeating timers.
+   --  -----------------------
+   procedure Test_Repeat_Timer (T : in out Test) is
+      M        : Timer_List;
+      R        : Timer_Ref;
+      Start    : constant Ada.Real_Time.Time := Ada.Real_Time.Clock;
+      Deadline : Ada.Real_Time.Time;
+      Now      : Ada.Real_Time.Time;
+   begin
+      T.Count := 0;
+      T.Repeat := 5;
+      for Retry in 1 .. 10 loop
+         M.Set_Timer (T'Unchecked_Access, R, Start + Ada.Real_Time.Milliseconds (10));
+         M.Process (Deadline);
+         Now := Ada.Real_Time.Clock;
+         exit when Now < Deadline;
+      end loop;
+      T.Assert (Now < Deadline, "The timer deadline is not correct");
+      loop
+         delay until Deadline;
+         M.Process (Deadline);
+         exit when Deadline >= Now + Ada.Real_Time.Seconds (1);
+      end loop;
+      Assert_Equals (T, 5, T.Count, "The timer handler was not repeated");
+   end Test_Repeat_Timer;
 
 end Util.Events.Timers.Tests;
