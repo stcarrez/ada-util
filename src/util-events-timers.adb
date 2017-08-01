ad1da3bc5ed08bebdb430e8764ac07f4481450af
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-08-01 21:22:15 +0200

    Implement the timer management operations

---
+++ src/util-events-timers.adb
@@ -0,0 +1,227 @@
+-----------------------------------------------------------------------
+--  util-events-timers -- Timer list management
+--  Copyright (C) 2017 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+with Ada.Unchecked_Deallocation;
+package body Util.Events.Timers is
+
+   use type Ada.Real_Time.Time;
+
+   procedure Free is
+     new Ada.Unchecked_Deallocation (Object => Timer_Node,
+                                     Name   => Timer_Node_Access);
+
+   --  Repeat the timer.
+   procedure Repeat (Event   : in out Timer_Ref;
+                     In_Time : in Ada.Real_Time.Time_Span) is
+   begin
+      null;
+   end Repeat;
+
+   --  -----------------------
+   --  Cancel the timer.
+   --  -----------------------
+   procedure Cancel (Event : in out Timer_Ref) is
+   begin
+      if Event.Value /= null and then Event.Value.List /= null then
+         Event.Value.List.all.Cancel (Event.Value);
+         Event.Value.List := null;
+      end if;
+   end Cancel;
+
+   --  -----------------------
+   --  Check if the timer is ready to be executed.
+   --  -----------------------
+   function Is_Scheduled (Event : in Timer_Ref) return Boolean is
+   begin
+      return Event.Value /= null and then Event.Value.List /= null;
+   end Is_Scheduled;
+
+   --  -----------------------
+   --  Returns the deadline time for the timer execution.
+   --  Returns Time'Last if the timer is not scheduled.
+   --  -----------------------
+   function Time_Of_Event (Event : in Timer_Ref) return Ada.Real_Time.Time is
+   begin
+      return (if Event.Value /= null then Event.Value.Deadline else Ada.Real_Time.Time_Last);
+   end Time_Of_Event;
+
+   --  -----------------------
+   --  Set a timer to be called at the given time.
+   --  -----------------------
+   procedure Set_Timer (List    : in out Timer_List;
+                        Handler : in Timer_Access;
+                        Event   : in out Timer_Ref'Class;
+                        At_Time : in Ada.Real_Time.Time) is
+   begin
+      if Event.Value = null then
+         Event.Value := new Timer_Node;
+      end if;
+      Event.Value.Handler := Handler;
+
+      --  Cancel the timer if it is part of another timer manager.
+      if Event.Value.List /= null and Event.Value.List /= List.Manager'Unchecked_Access then
+         Event.Value.List.all.Cancel (Event.Value);
+      end if;
+
+      --  Update the timer.
+      Event.Value.List := List.Manager'Unchecked_Access;
+      List.Manager.Add (Event.Value, At_Time);
+   end Set_Timer;
+
+   --  -----------------------
+   --  Process the timer handlers that have passed the deadline and return the next
+   --  deadline.  The <tt>Max_Count</tt> parameter allows to limit the number of timer handlers
+   --  that are called by operation.  The default is not limited.
+   --  -----------------------
+   procedure Process (List      : in out Timer_List;
+                      Timeout   : out Ada.Real_Time.Time;
+                      Max_Count : in Natural := Natural'Last) is
+      Timer : Timer_Ref;
+   begin
+      loop
+         List.Manager.Find_Next (Timeout, Timer);
+         exit when not Timer.Is_Scheduled;
+         Timer.Value.Handler.Time_Handler (Timer);
+      end loop;
+   end Process;
+
+   overriding
+   procedure Adjust (Object : in out Timer_Ref) is
+   begin
+      if Object.Value /= null then
+         Util.Concurrent.Counters.Increment (Object.Value.Counter);
+      end if;
+   end Adjust;
+
+   overriding
+   procedure Finalize (Object : in out Timer_Ref) is
+      Is_Zero : Boolean;
+   begin
+      if Object.Value /= null then
+         Util.Concurrent.Counters.Decrement (Object.Value.Counter, Is_Zero);
+         if Is_Zero then
+            Free (Object.Value);
+         else
+            Object.Value := null;
+         end if;
+      end if;
+   end Finalize;
+
+   protected body Timer_Manager is
+
+      --  -----------------------
+      --  Add a timer.
+      --  -----------------------
+      procedure Add (Timer    : in out Timer_Node_Access;
+                     Deadline : in Ada.Real_Time.Time) is
+         Current : Timer_Node_Access := List;
+         Prev    : Timer_Node_Access;
+      begin
+         Util.Concurrent.Counters.Increment (Timer.Counter);
+         if Timer.List /= null then
+            Cancel (Timer);
+         end if;
+         Timer.Deadline := Deadline;
+         while Current /= null loop
+            if Current.Deadline < Deadline then
+               if Prev = null then
+                  List := Timer;
+               else
+                  Prev.Next := Timer;
+               end if;
+               Timer.Next := Current;
+               Current.Prev := Timer;
+               return;
+            end if;
+            Prev := Current;
+            Current := Current.Next;
+         end loop;
+         if Prev = null then
+            List := Timer;
+            Timer.Prev := null;
+         else
+            Prev.Next := Timer;
+            Timer.Prev := Prev;
+         end if;
+         Timer.Next := null;
+      end Add;
+
+      --  -----------------------
+      --  Cancel a timer.
+      --  -----------------------
+      procedure Cancel (Timer : in out Timer_Node_Access) is
+         Is_Zero : Boolean;
+      begin
+         if Timer.List = null then
+            return;
+         end if;
+         if List = Timer then
+            List := Timer.Next;
+            Timer.Prev := null;
+            if List /= null then
+               List.Prev := null;
+            end if;
+         elsif Timer.Prev /= null then
+            Timer.Prev.Next := Timer.Next;
+            Timer.Next.Prev := Timer.Prev;
+         else
+            return;
+         end if;
+         Timer.Next := null;
+         Timer.Prev := null;
+         Timer.List := null;
+         Util.Concurrent.Counters.Decrement (Timer.Counter, Is_Zero);
+         if Is_Zero then
+            Free (Timer);
+         end if;
+      end Cancel;
+
+      --  -----------------------
+      --  Find the next timer to be executed or return the next deadline.
+      --  -----------------------
+      procedure Find_Next (Deadline : out Ada.Real_Time.Time;
+                           Timer    : in out Timer_Ref) is
+         Now : constant Ada.Real_Time.Time := Ada.Real_Time.Clock;
+      begin
+         if List = null then
+            Timer.Finalize;
+            Deadline := Ada.Real_Time.Time_Last;
+         elsif List.Deadline < Now then
+            Timer.Value := List;
+            Util.Concurrent.Counters.Increment (Timer.Value.Counter);
+            List := List.Next;
+            if List /= null then
+               List.Prev := null;
+               Deadline := List.Deadline;
+            else
+               Deadline := Ada.Real_Time.Time_Last;
+            end if;
+         else
+            Deadline := List.Deadline;
+            Timer.Finalize;
+         end if;
+      end Find_Next;
+
+   end Timer_Manager;
+
+   overriding
+   procedure Finalize (Object : in out Timer_List) is
+   begin
+      null;
+   end Finalize;
+
+end Util.Events.Timers;
