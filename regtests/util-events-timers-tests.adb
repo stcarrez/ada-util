5c106d6d731043c44152c504e1b4e4228a9a23e4
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-08-01 21:33:18 +0200

    Implement the new unit tests for the timer management

---
+++ regtests/util-events-timers-tests.adb
@@ -0,0 +1,62 @@
+-----------------------------------------------------------------------
+--  util-events-timers-tests -- Unit tests for timers
+--  Copyright (C) 2017 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+with Util.Tests;
+with Util.Test_Caller;
+
+package body Util.Events.Timers.Tests is
+
+   use Util.Tests;
+   use type Ada.Real_Time.Time;
+
+   package Caller is new Util.Test_Caller (Test, "Events.Timers");
+
+   procedure Add_Tests (Suite : in Util.Tests.Access_Test_Suite) is
+   begin
+      Caller.Add_Test (Suite, "Test Util.Events.Channels.Post_Event",
+                       Test_Timer_Event'Access);
+   end Add_Tests;
+
+   overriding
+   procedure Time_Handler (Sub   : in out Test;
+                           Event : in out Timer_Ref'Class) is
+   begin
+      Sub.Count := Sub.Count + 1;
+   end Time_Handler;
+
+   procedure Test_Timer_Event (T : in out Test) is
+      M        : Timer_List;
+      R        : Timer_Ref;
+      Start    : Ada.Real_Time.Time := Ada.Real_Time.Clock;
+      Deadline : Ada.Real_Time.Time;
+      Now      : Ada.Real_Time.Time;
+   begin
+      T.Assert (not R.Is_Scheduled, "Empty timer should not be scheduled");
+      for Retry in 1 .. 10 loop
+         M.Set_Timer (T'Unchecked_Access, R, Start + Ada.Real_Time.Milliseconds (10));
+         M.Process (Deadline);
+         Now := Ada.Real_Time.Clock;
+         exit when Now < Deadline;
+      end loop;
+      T.Assert (Now < Deadline, "The timer deadline is not correct");
+      delay until Deadline;
+      M.Process (Deadline);
+      Assert_Equals (T, 1, T.Count, "The timer handler was not called");
+   end Test_Timer_Event;
+
+end Util.Events.Timers.Tests;
