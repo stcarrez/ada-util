614f75c5eb70a5934233385ed8c6a2d18f06e18a
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-09-29 23:15:11 +0200

    Move the Start_Object, Finish_Object, Start_Array, End_Array, Push Pop, Set_Member operations to the Mappers package Use the Reader interface object

--- src/util-serialize-io.adb
+++ src/util-serialize-io.adb
@@ -46,7 +46,8 @@ package body Util.Serialize.IO is
    --  Read the file and parse it using the JSON parser.
    --  ------------------------------
    procedure Parse (Handler : in out Parser;
-                    File    : in String) is
+                    File    : in String;
+                    Sink    : in out Reader'Class) is
       Stream     : aliased Util.Streams.Files.File_Stream;
       Buffer     : Util.Streams.Buffered.Buffered_Stream;
    begin
@@ -60,25 +61,28 @@ package body Util.Serialize.IO is
                          Input  => Stream'Unchecked_Access,
                          Size   => 1024);
       Stream.Open (Mode => Ada.Streams.Stream_IO.In_File, Name => File);
-      Context_Stack.Clear (Handler.Stack);
-      Parser'Class (Handler).Parse (Buffer);
+      --  Context_Stack.Clear (Handler.Stack);
+      Parser'Class (Handler).Parse (Buffer, Sink);
 
    exception
-      when Util.Serialize.Mappers.Field_Fatal_Error =>
-         null;
+--      when Util.Serialize.Mappers.Field_Fatal_Error =>
+--         null;
 
       when Ada.IO_Exceptions.Name_Error =>
          Parser'Class (Handler).Error ("File '" & File & "' does not exist.");
 
       when E : others =>
-         Parser'Class (Handler).Error ("Exception " & Ada.Exceptions.Exception_Name (E));
+         if not Handler.Error_Flag then
+            Parser'Class (Handler).Error ("Exception " & Ada.Exceptions.Exception_Name (E));
+         end if;
    end Parse;
 
    --  ------------------------------
    --  Parse the content string.
    --  ------------------------------
    procedure Parse_String (Handler : in out Parser;
-                           Content : in String) is
+                           Content : in String;
+                           Sink    : in out Reader'Class) is
       Stream : aliased Util.Streams.Buffered.Buffered_Stream;
    begin
       if Handler.Error_Logger = null then
@@ -86,15 +90,18 @@ package body Util.Serialize.IO is
       end if;
       Handler.File := Ada.Strings.Unbounded.To_Unbounded_String ("<inline>");
       Stream.Initialize (Content  => Content);
-      Context_Stack.Clear (Handler.Stack);
-      Parser'Class (Handler).Parse (Stream);
+      --  Context_Stack.Clear (Handler.Stack);
+      Parser'Class (Handler).Parse (Stream, Sink);
 
    exception
-      when Util.Serialize.Mappers.Field_Fatal_Error =>
-         null;
+--      when Util.Serialize.Mappers.Field_Fatal_Error =>
+--         null;
 
       when E : others =>
-         Parser'Class (Handler).Error ("Exception " & Ada.Exceptions.Exception_Name (E));
+         if not Handler.Error_Flag then
+            Parser'Class (Handler).Error ("Exception " & Ada.Exceptions.Exception_Name (E));
+         end if;
+
    end Parse_String;
 
    --  ------------------------------
@@ -113,170 +120,6 @@ package body Util.Serialize.IO is
    begin
       Handler.Error_Logger := Logger;
    end Set_Logger;
-
-   --  ------------------------------
-   --  Push the current context when entering in an element.
-   --  ------------------------------
-   procedure Push (Handler : in out Parser) is
-      use type Util.Serialize.Mappers.Mapper_Access;
-   begin
-      Context_Stack.Push (Handler.Stack);
-   end Push;
-
-   --  ------------------------------
-   --  Pop the context and restore the previous context when leaving an element
-   --  ------------------------------
-   procedure Pop (Handler  : in out Parser) is
-   begin
-      Context_Stack.Pop (Handler.Stack);
-   end Pop;
-
-   function Find_Mapper (Handler : in Parser;
-                         Name    : in String) return Util.Serialize.Mappers.Mapper_Access is
-      pragma Unreferenced (Handler, Name);
-   begin
-      return null;
-   end Find_Mapper;
-
-   --  ------------------------------
-   --  Start a new object associated with the given name.  This is called when
-   --  the '{' is reached.  The reader must be updated so that the next
-   --  <b>Set_Member</b> procedure will associate the name/value pair on the
-   --  new object.
-   --  ------------------------------
-   procedure Start_Object (Handler : in out Parser;
-                           Name    : in String) is
-
-      use type Util.Serialize.Mappers.Mapper_Access;
-
-      Current : constant Element_Context_Access := Context_Stack.Current (Handler.Stack);
-      Next    : Element_Context_Access;
-      Pos     : Positive;
-   begin
-      Log.Debug ("Start object {0}", Name);
-
-      Context_Stack.Push (Handler.Stack);
-      Next := Context_Stack.Current (Handler.Stack);
-      if Current /= null then
-         Pos := 1;
-
-         --  Notify we are entering in the given node for each active mapping.
-         for I in Current.Active_Nodes'Range loop
-            declare
-               Node  : constant Mappers.Mapper_Access := Current.Active_Nodes (I);
-               Child : Mappers.Mapper_Access;
-            begin
-               exit when Node = null;
-               Child := Node.Find_Mapper (Name => Name);
-               if Child = null and then Node.Is_Wildcard then
-                  Child := Node;
-               end if;
-               if Child /= null then
-                  Log.Debug ("{0} is matching {1}", Name, Child.Get_Name);
-                  Child.Start_Object (Handler, Name);
-                  Next.Active_Nodes (Pos) := Child;
-                  Pos := Pos + 1;
-               end if;
-            end;
-         end loop;
-         while Pos <= Next.Active_Nodes'Last loop
-            Next.Active_Nodes (Pos) := null;
-            Pos := Pos + 1;
-         end loop;
-      else
-         Next.Active_Nodes (1) := Handler.Mapping_Tree.Find_Mapper (Name);
-      end if;
-   end Start_Object;
-
-   --  ------------------------------
-   --  Finish an object associated with the given name.  The reader must be
-   --  updated to be associated with the previous object.
-   --  ------------------------------
-   procedure Finish_Object (Handler : in out Parser;
-                            Name    : in String) is
-
-      use type Util.Serialize.Mappers.Mapper_Access;
-   begin
-      Log.Debug ("Finish object {0}", Name);
-
-      declare
-         Current : constant Element_Context_Access := Context_Stack.Current (Handler.Stack);
-      begin
-         if Current /= null then
-            --  Notify we are leaving the given node for each active mapping.
-            for I in Current.Active_Nodes'Range loop
-               declare
-                  Node : constant Mappers.Mapper_Access := Current.Active_Nodes (I);
-               begin
-                  exit when Node = null;
-                  Node.Finish_Object (Handler, Name);
-               end;
-            end loop;
-         end if;
-      end;
-      Handler.Pop;
-   end Finish_Object;
-
-   procedure Start_Array (Handler : in out Parser;
-                          Name    : in String) is
-      pragma Unreferenced (Name);
-   begin
-      Handler.Push;
-   end Start_Array;
-
-   procedure Finish_Array (Handler : in out Parser;
-                           Name    : in String;
-                           Count   : in Natural) is
-      pragma Unreferenced (Name, Count);
-   begin
-      Handler.Pop;
-   end Finish_Array;
-
-   --  -----------------------
-   --  Set the name/value pair on the current object.  For each active mapping,
-   --  find whether a rule matches our name and execute it.
-   --  -----------------------
-   procedure Set_Member (Handler   : in out Parser;
-                         Name      : in String;
-                         Value     : in Util.Beans.Objects.Object;
-                         Attribute : in Boolean := False) is
-      use Util.Serialize.Mappers;
-
-      Current : constant Element_Context_Access := Context_Stack.Current (Handler.Stack);
-   begin
-      Log.Debug ("Set member {0}", Name);
-
-      if Current /= null then
-
-         --  Look each active mapping node.
-         for I in Current.Active_Nodes'Range loop
-            declare
-               Node : constant Mapper_Access := Current.Active_Nodes (I);
-            begin
-               exit when Node = null;
-               Node.Set_Member (Name      => Name,
-                                Value     => Value,
-                                Attribute => Attribute,
-                                Context   => Handler);
-
-            exception
-               when E : Util.Serialize.Mappers.Field_Error =>
-                  Parser'Class (Handler).Error (Message => Ada.Exceptions.Exception_Message (E));
-
-               when E : Util.Serialize.Mappers.Field_Fatal_Error =>
-                  Parser'Class (Handler).Error (Message => Ada.Exceptions.Exception_Message (E));
-                  raise;
-
-                  --  For other exception, report an error with the field name and value.
-               when E : others =>
-                  Parser'Class (Handler).Error (Message => "Cannot set field '" & Name & "' to '"
-                                                & Util.Beans.Objects.To_String (Value) & "': "
-                                                & Ada.Exceptions.Exception_Message (E));
-                  raise;
-            end;
-         end loop;
-      end if;
-   end Set_Member;
 
    --  ------------------------------
    --  Get the current location (file and line) to report an error message.
@@ -299,21 +142,5 @@ package body Util.Serialize.IO is
                                   Message);
       Handler.Error_Flag := True;
    end Error;
-
-   procedure Add_Mapping (Handler : in out Parser;
-                          Path    : in String;
-                          Mapper  : in Util.Serialize.Mappers.Mapper_Access) is
-   begin
-      Handler.Mapping_Tree.Add_Mapping (Path, Mapper);
-   end Add_Mapping;
-
-   --  ------------------------------
-   --  Dump the mapping tree on the logger using the INFO log level.
-   --  ------------------------------
-   procedure Dump (Handler : in Parser'Class;
-                   Logger  : in Util.Log.Loggers.Logger'Class) is
-   begin
-      Util.Serialize.Mappers.Dump (Handler.Mapping_Tree, Logger, "Mapping ");
-   end Dump;
 
 end Util.Serialize.IO;
