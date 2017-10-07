aa898c2a6f6ff5a7d83457db975cb8ceb45abc97
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-10-07 16:31:40 +0200

    Override the Do_Delete procedure

--- src/http/curl/util-http-clients-curl.ads
+++ src/http/curl/util-http-clients-curl.ads
@@ -90,6 +90,12 @@ private
                      Data     : in String;
                      Reply    : out Response'Class);
 
+   overriding
+   procedure Do_Delete (Manager  : in Curl_Http_Manager;
+                        Http     : in Client'Class;
+                        URI      : in String;
+                        Reply    : out Response'Class);
+
    --  Set the timeout for the connection.
    overriding
    procedure Set_Timeout (Manager : in Curl_Http_Manager;
