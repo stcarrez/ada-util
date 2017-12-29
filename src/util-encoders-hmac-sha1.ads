89f1cdb0a0184907eac8ad4d74023bae47368359
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-12-29 17:43:47 +0100

    Change the Transformer interface to accept in out parameter for the Transformer

--- src/util-encoders-hmac-sha1.ads
+++ src/util-encoders-hmac-sha1.ads
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  util-encoders-hmac-sha1 -- Compute HMAC-SHA1 authentication code
---  Copyright (C) 2011, 2012 Stephane Carrez
+--  Copyright (C) 2011, 2012, 2017 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -99,7 +99,7 @@ package Util.Encoders.HMAC.SHA1 is
    --  The <b>Encoding_Error</b> exception is raised if the input
    --  stream cannot be transformed.
    overriding
-   procedure Transform (E       : in Encoder;
+   procedure Transform (E       : in out Encoder;
                         Data    : in Ada.Streams.Stream_Element_Array;
                         Into    : out Ada.Streams.Stream_Element_Array;
                         Last    : out Ada.Streams.Stream_Element_Offset;
