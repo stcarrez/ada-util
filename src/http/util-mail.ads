975223a03cbdbd528fa85b840781ed7b19653cfb
Stephane Carrez <Stephane.Carrez@gmail.com>
2018-01-12 08:36:22 +0100

    Declare the Get_First_Name function

--- src/http/util-mail.ads
+++ src/http/util-mail.ads
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  util-mail -- Mail Utility Library
---  Copyright (C) 2017 Stephane Carrez
+--  Copyright (C) 2017, 2018 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -29,5 +29,8 @@ package Util.Mail is
 
    --  Parse the email address and separate the name from the address.
    function Parse_Address (E_Mail : in String) return Email_Address;
+
+   --  Extract a first name from the email address.
+   function Get_First_Name (From : in Email_Address) return String;
 
 end Util.Mail;
