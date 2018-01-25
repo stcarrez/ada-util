f8d6406fe694b495263769383cd3b98f214d5ff4
Stephane Carrez <Stephane.Carrez@gmail.com>
2018-01-25 21:26:53 +0100

    New package for the serialization and parsing of application/x-www-form-urlencoded

---
+++ src/util-serialize-io-form.ads
@@ -0,0 +1,148 @@
+-----------------------------------------------------------------------
+--  util-serialize-io-form -- x-www-form-urlencoded streams
+--  Copyright (C) 2018 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+with Ada.Streams;
+with Util.Streams.Texts;
+with Util.Serialize.IO;
+package Util.Serialize.IO.Form is
+
+   type Output_Stream is limited new Util.Serialize.IO.Output_Stream with private;
+
+   procedure Initialize (Stream : in out Output_Stream;
+                         Output : in Util.Streams.Texts.Print_Stream_Access);
+
+   --  Flush the buffer (if any) to the sink.
+   overriding
+   procedure Flush (Stream : in out Output_Stream);
+
+   --  Close the sink.
+   overriding
+   procedure Close (Stream : in out Output_Stream);
+
+   --  Write the buffer array to the output stream.
+   overriding
+   procedure Write (Stream : in out Output_Stream;
+                    Buffer : in Ada.Streams.Stream_Element_Array);
+
+   --  Write the attribute name/value pair.
+   overriding
+   procedure Write_Attribute (Stream : in out Output_Stream;
+                              Name   : in String;
+                              Value  : in String);
+
+   overriding
+   procedure Write_Wide_Attribute (Stream : in out Output_Stream;
+                                   Name   : in String;
+                                   Value  : in Wide_Wide_String);
+
+   overriding
+   procedure Write_Attribute (Stream : in out Output_Stream;
+                              Name   : in String;
+                              Value  : in Integer);
+
+   overriding
+   procedure Write_Attribute (Stream : in out Output_Stream;
+                              Name   : in String;
+                              Value  : in Boolean);
+
+   overriding
+   procedure Write_Attribute (Stream : in out Output_Stream;
+                              Name   : in String;
+                              Value  : in Util.Beans.Objects.Object);
+
+   --  Write the attribute with a null value.
+   overriding
+   procedure Write_Null_Attribute (Stream : in out Output_Stream;
+                                   Name   : in String);
+
+   --  Write the entity value.
+   overriding
+   procedure Write_Entity (Stream : in out Output_Stream;
+                           Name   : in String;
+                           Value  : in String);
+
+   overriding
+   procedure Write_Wide_Entity (Stream : in out Output_Stream;
+                                Name   : in String;
+                                Value  : in Wide_Wide_String);
+
+   overriding
+   procedure Write_Entity (Stream : in out Output_Stream;
+                           Name   : in String;
+                           Value  : in Boolean);
+
+   overriding
+   procedure Write_Entity (Stream : in out Output_Stream;
+                           Name   : in String;
+                           Value  : in Integer);
+
+   overriding
+   procedure Write_Entity (Stream : in out Output_Stream;
+                           Name   : in String;
+                           Value  : in Ada.Calendar.Time);
+
+   overriding
+   procedure Write_Long_Entity (Stream : in out Output_Stream;
+                                Name   : in String;
+                                Value  : in Long_Long_Integer);
+
+   overriding
+   procedure Write_Enum_Entity (Stream : in out Output_Stream;
+                                Name   : in String;
+                                Value  : in String);
+
+   overriding
+   procedure Write_Entity (Stream : in out Output_Stream;
+                           Name   : in String;
+                           Value  : in Util.Beans.Objects.Object);
+
+   --  Write an entity with a null value.
+   overriding
+   procedure Write_Null_Entity (Stream : in out Output_Stream;
+                                Name   : in String);
+
+   type Parser is new Serialize.IO.Parser with private;
+
+   --  Parse the stream using the form parser.
+   procedure Parse (Handler : in out Parser;
+                    Stream  : in out Util.Streams.Buffered.Input_Buffer_Stream'Class;
+                    Sink    : in out Reader'Class);
+
+   --  Get the current location (file and line) to report an error message.
+   function Get_Location (Handler : in Parser) return String;
+
+   --  Read a form file and return an object.
+   function Read (Path : in String) return Util.Beans.Objects.Object;
+
+private
+
+   procedure Write_Escape (Stream : in out Output_Stream;
+                           Value  : in String);
+
+   type Output_Stream is limited new Util.Serialize.IO.Output_Stream with record
+      Stream    : Util.Streams.Texts.Print_Stream_Access;
+      Has_Param : Boolean := False;
+   end record;
+
+   type Parser is new Util.Serialize.IO.Parser with record
+      Token            : Ada.Strings.Unbounded.Unbounded_String;
+      Line_Number      : Natural  := 1;
+      Has_Pending_Char : Boolean := False;
+      Pending_Char     : Character;
+   end record;
+
+end Util.Serialize.IO.Form;
