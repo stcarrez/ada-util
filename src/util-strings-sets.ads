661ab157f5970105f7dd6883b1558897050dd4d0
Stephane Carrez <Stephane.Carrez@gmail.com>
2018-02-04 21:39:06 +0100

    Fix header style

--- src/util-strings-sets.ads
+++ src/util-strings-sets.ads
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
---  Util-strings-sets --  Set of strings
---  Copyright (C) 2011 Stephane Carrez
+--  util-strings-sets --  Set of strings
+--  Copyright (C) 2011, 2018 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
