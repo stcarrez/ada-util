6867eee52ad4b87a4d66af1c729174d95dcd7fc1
Stephane Carrez <Stephane.Carrez@gmail.com>
2018-02-04 21:40:30 +0100

    Fix header style

--- src/util-events-channels.adb
+++ src/util-events-channels.adb
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
---  events-channel -- Event Channels
---  Copyright (C) 2001, 2002, 2003, 2009, 2010 Stephane Carrez
+--  util-events-channel -- Event Channels
+--  Copyright (C) 2001, 2002, 2003, 2009, 2010, 2018 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
