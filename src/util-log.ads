4aa628b2af4a47c68d19a0a204d4f03cd3dab1d0
Stephane Carrez <Stephane.Carrez@gmail.com>
2018-02-04 21:40:01 +0100

    Fix header style

--- src/util-log.ads
+++ src/util-log.ads
@@ -1,5 +1,5 @@
 -----------------------------------------------------------------------
---  Logs -- Utility Log Package
+--  util-log -- Utility Log Package
 --  Copyright (C) 2001, 2002, 2003, 2006, 2008, 2009, 2010, 2011, 2017, 2018 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
