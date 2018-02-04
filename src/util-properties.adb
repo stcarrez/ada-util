fac1c564313a896003a16aa987792b6bfde01099
Stephane Carrez <Stephane.Carrez@gmail.com>
2018-02-04 21:39:29 +0100

    Fix header style

--- src/util-properties.adb
+++ src/util-properties.adb
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
---  properties -- Generic name/value property management
---  Copyright (C) 2001, 2002, 2003, 2006, 2008, 2009, 2010, 2011, 2012, 2014, 2017 Stephane Carrez
+--  util-properties -- Generic name/value property management
+--  Copyright (C) 2001 - 2018 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
