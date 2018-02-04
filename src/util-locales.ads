22d8d45e7a3cda6e5365364c0d32d633a1434f13
Stephane Carrez <Stephane.Carrez@gmail.com>
2018-02-04 21:40:24 +0100

    Fix header style

--- src/util-locales.ads
+++ src/util-locales.ads
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
---  Util.Locales -- Locale
---  Copyright (C) 2001, 2002, 2003, 2009, 2010, 2011, 2012, 2013 Stephane Carrez
+--  util-locales -- Locale support
+--  Copyright (C) 2001, 2002, 2003, 2009, 2010, 2011, 2012, 2013, 2018 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
