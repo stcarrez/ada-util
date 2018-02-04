67c629891a230f90db5988cf195e0508acb3df9c
Stephane Carrez <Stephane.Carrez@gmail.com>
2018-02-04 21:40:53 +0100

    Fix header style

--- src/util-beans-methods.ads
+++ src/util-beans-methods.ads
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
---  Util.Beans.Methods -- Bean methods
---  Copyright (C) 2010, 2011 Stephane Carrez
+--  util-beans-methods -- Bean methods
+--  Copyright (C) 2010, 2011, 2018 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
