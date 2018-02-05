b02dad30fc9fed9397ab453ce179d68b4bfb5336
Stephane Carrez <Stephane.Carrez@gmail.com>
2018-02-05 23:34:26 +0100

    Integrate some documentation about Ada beans

--- src/util-beans.ads
+++ src/util-beans.ads
@@ -17,6 +17,29 @@
 -----------------------------------------------------------------------
 
 --  = Ada Beans =
+--  A [Java Bean](http://en.wikipedia.org/wiki/JavaBean) is an object that
+--  allows to access its properties through getters and setters. Java Beans
+--  rely on the use of Java introspection to discover the Java Bean object properties.
+--
+--  An Ada Bean has some similarities with the Java Bean as it tries to expose
+--  an object through a set of common interfaces.  Since Ada does not have introspection,
+--  some developer work is necessary. The Ada Bean framework consists of:
+--
+--    * An `Object` concrete type that allows to hold any data type such
+--      as boolean, integer, floats, strings, dates and Ada bean objects.
+--    * A `Bean` interface that exposes a `Get_Value` and `Set_Value`
+--      operation through which the object properties can be obtained and modified.
+--    * A `Method_Bean` interface that exposes a set of method bindings
+--      that gives access to the methods provided by the Ada Bean object.
+--
+--  The benefit of Ada beans comes when you need to get a value or invoke
+--  a method on an object but you don't know at compile time the object or method.
+--  That step being done later through some external configuration or presentation file.
+--
+--  The Ada Bean framework is the basis for the implementation of
+--  [Ada Server Faces](http://code.google.com/p/ada-asf/) and
+--  [Ada EL](http://code.google.com/p/ada-el/).  It allows the presentation layer to
+--  access information provided by Ada beans.
 --
 --  @include util-beans-objects.ads
 --  @include util-beans-objects-datasets.ads
