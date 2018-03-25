bc4445378b6f457993c62077a3dfe45f8e8c36d3
Stephane Carrez <Stephane.Carrez@gmail.com>
2018-03-25 15:05:55 +0200

    Add tests for the Util.Commands.Drivers package

---
+++ regtests/util-commands-tests.ads
@@ -0,0 +1,35 @@
+-----------------------------------------------------------------------
+--  util-commands-tests - Test for commands
+--  Copyright (C) 2018 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+with Util.Tests;
+package Util.Commands.Tests is
+
+   procedure Add_Tests (Suite : in Util.Tests.Access_Test_Suite);
+
+   type Test is new Util.Tests.Test with null record;
+
+   --  Tests when the execution of commands.
+   procedure Test_Execute (T : in out Test);
+
+   --  Test execution of help.
+   procedure Test_Help (T : in out Test);
+
+   --  Test usage operation.
+   procedure Test_Usage (T : in out Test);
+
+end Util.Commands.Tests;
