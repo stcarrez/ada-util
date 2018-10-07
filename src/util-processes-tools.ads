95880958f105d4b3a56bfffe99308c2e53c9554b
Stephane Carrez <Stephane.Carrez@gmail.com>
2018-10-07 21:15:15 +0200

    Declare more Execute procedures as helper to launch a command and get the output

--- src/util-processes-tools.ads
+++ src/util-processes-tools.ads
@@ -17,13 +17,23 @@
 -----------------------------------------------------------------------
 
 with Util.Strings.Vectors;
-
+with Util.Streams.Pipes;
 package Util.Processes.Tools is
 
    --  Execute the command and append the output in the vector array.
    --  The program output is read line by line and the standard input is closed.
    --  Return the program exit status.
    procedure Execute (Command : in String;
+                      Output  : in out Util.Strings.Vectors.Vector;
+                      Status  : out Integer);
+
+   procedure Execute (Command    : in String;
+                      Input_Path : in String;
+                      Output     : in out Util.Strings.Vectors.Vector;
+                      Status     : out Integer);
+
+   procedure Execute (Command : in String;
+                      Process : in out Util.Streams.Pipes.Pipe_Stream;
                       Output  : in out Util.Strings.Vectors.Vector;
                       Status  : out Integer);
 
