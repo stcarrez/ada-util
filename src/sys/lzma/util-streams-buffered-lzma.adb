36820ea264798a253dd377f3d8d46da4e4bb5407
Stephane Carrez <Stephane.Carrez@gmail.com>
2019-03-17 09:08:00 +0100

    Implement operations for Decompress_Stream to decompress LZMA streams

--- src/sys/lzma/util-streams-buffered-lzma.adb
+++ src/sys/lzma/util-streams-buffered-lzma.adb
@@ -18,11 +18,13 @@
 with Lzma.Check;
 with Lzma.Container;
 with Interfaces.C;
+with Ada.IO_Exceptions;
 package body Util.Streams.Buffered.Lzma is
 
    use type Interfaces.C.size_t;
    use type Base.lzma_ret;
 
+   subtype size_t is Interfaces.C.size_t;
    subtype Offset is Ada.Streams.Stream_Element_Offset;
 
    --  -----------------------
@@ -31,7 +33,7 @@ package body Util.Streams.Buffered.Lzma is
    --  -----------------------
    overriding
    procedure Initialize (Stream  : in out Compress_Stream;
-                         Output  : in Output_Stream_Access;
+                         Output  : access Output_Stream'Class;
                          Size    : in Positive) is
       Result : Base.lzma_ret;
    begin
@@ -41,6 +43,9 @@ package body Util.Streams.Buffered.Lzma is
       Stream.Stream.avail_out := Stream.Buffer'Length;
       Result := Container.lzma_easy_encoder (Stream.Stream'Unchecked_Access, 6,
                                              Check.LZMA_CHECK_CRC64);
+      if Result /= Base.LZMA_OK then
+         raise Ada.IO_Exceptions.Device_Error with "Cannot initialize compressor";
+      end if;
    end Initialize;
 
    --  -----------------------
@@ -50,7 +55,7 @@ package body Util.Streams.Buffered.Lzma is
    procedure Close (Stream : in out Compress_Stream) is
    begin
       Stream.Flush;
-      Stream.Output.Close;
+      Output_Buffer_Stream (Stream).Close;
    end Close;
 
    --  -----------------------
@@ -66,7 +71,7 @@ package body Util.Streams.Buffered.Lzma is
       loop
          if Stream.Stream.avail_in = 0 then
             Stream.Stream.next_in := Buffer (Buffer'First)'Unrestricted_Access;
-            Stream.Stream.avail_in := Interfaces.C.size_t (Buffer'Length);
+            Stream.Stream.avail_in := size_t (Buffer'Length);
             Encoded := True;
          end if;
 
@@ -115,7 +120,80 @@ package body Util.Streams.Buffered.Lzma is
    overriding
    procedure Finalize (Object : in out Compress_Stream) is
    begin
-      null;
+      Base.lzma_end (Object.Stream'Unchecked_Access);
+      Output_Buffer_Stream (Object).Finalize;
+   end Finalize;
+
+   --  -----------------------
+   --  Initialize the stream to write on the given stream.
+   --  An internal buffer is allocated for writing the stream.
+   --  -----------------------
+   overriding
+   procedure Initialize (Stream  : in out Decompress_Stream;
+                         Input   : access Input_Stream'Class;
+                         Size    : in Positive) is
+      Result : Base.lzma_ret;
+   begin
+      Input_Buffer_Stream (Stream).Initialize (Input, Size);
+
+      Stream.Stream.next_in := Stream.Buffer (Stream.Buffer'First)'Unchecked_Access;
+      Stream.Stream.avail_in := 0;
+      Result := Container.lzma_stream_decoder (Stream.Stream'Unchecked_Access,
+                                               Long_Long_Integer'Last,
+                                               Container.LZMA_CONCATENATED);
+      if Result /= Base.LZMA_OK then
+         raise Ada.IO_Exceptions.Device_Error with "Cannot initialize decompressor";
+      end if;
+   end Initialize;
+
+   --  -----------------------
+   --  Write the buffer array to the output stream.
+   --  -----------------------
+   overriding
+   procedure Read (Stream : in out Decompress_Stream;
+                   Into   : out Ada.Streams.Stream_Element_Array;
+                   Last   : out Ada.Streams.Stream_Element_Offset) is
+      use type Base.lzma_action;
+
+      Result : Base.lzma_ret;
+   begin
+      Stream.Stream.next_out  := Into (Into'First)'Unrestricted_Access;
+      Stream.Stream.avail_out := Into'Length;
+      loop
+         if Stream.Stream.avail_in = 0 and not Stream.Is_Eof
+           and Stream.Action = Base.LZMA_RUN
+         then
+            Stream.Fill;
+            if Stream.Write_Pos >= Stream.Read_Pos then
+               Stream.Stream.avail_in := size_t (Stream.Write_Pos - Stream.Read_Pos);
+               Stream.Stream.next_in := Stream.Buffer (Stream.Buffer'First)'Unchecked_Access;
+            else
+               Stream.Stream.avail_in := 0;
+               Stream.Stream.next_in := null;
+               Stream.Action := Base.LZMA_FINISH;
+            end if;
+         end if;
+         Result := Base.lzma_code (Stream.Stream'Unchecked_Access, Stream.Action);
+
+         if Stream.Stream.avail_out = 0 or Result = Base.LZMA_STREAM_END then
+            Last := Into'First + Into'Length
+              - Offset (Stream.Stream.avail_out) - 1;
+            return;
+         end if;
+         if Result /= Base.LZMA_OK then
+            raise Ada.IO_Exceptions.Data_Error with "Decompression error";
+         end if;
+      end loop;
+   end Read;
+
+   --  -----------------------
+   --  Flush the stream and release the buffer.
+   --  -----------------------
+   overriding
+   procedure Finalize (Object : in out Decompress_Stream) is
+   begin
+      Base.lzma_end (Object.Stream'Unchecked_Access);
+      Input_Buffer_Stream (Object).Finalize;
    end Finalize;
 
 end Util.Streams.Buffered.Lzma;
