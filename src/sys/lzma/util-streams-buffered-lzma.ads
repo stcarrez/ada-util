217219f906d8ddd9e67adac48e71551a658a4dc2
Stephane Carrez <Stephane.Carrez@gmail.com>
2019-03-17 09:06:56 +0100

    Declare Decompress_Stream type and associated operations for LZMA stream decompression

--- src/sys/lzma/util-streams-buffered-lzma.ads
+++ src/sys/lzma/util-streams-buffered-lzma.ads
@@ -31,7 +31,7 @@ package Util.Streams.Buffered.Lzma is
    --  An internal buffer is allocated for writing the stream.
    overriding
    procedure Initialize (Stream  : in out Compress_Stream;
-                         Output  : in Output_Stream_Access;
+                         Output  : access Output_Stream'Class;
                          Size    : in Positive);
 
    --  Close the sink.
@@ -48,6 +48,26 @@ package Util.Streams.Buffered.Lzma is
    overriding
    procedure Flush (Stream : in out Compress_Stream);
 
+   --  -----------------------
+   --  Compress stream
+   --  -----------------------
+   --  The <b>Compress_Stream</b> is an output stream which uses the LZMA encoder to
+   --  compress the data before writing it to the output.
+   type Decompress_Stream is limited new Util.Streams.Buffered.Input_Buffer_Stream with private;
+
+   --  Initialize the stream to write on the given stream.
+   --  An internal buffer is allocated for writing the stream.
+   overriding
+   procedure Initialize (Stream  : in out Decompress_Stream;
+                         Input   : access Input_Stream'Class;
+                         Size    : in Positive);
+
+   --  Write the buffer array to the output stream.
+   overriding
+   procedure Read (Stream : in out Decompress_Stream;
+                   Into   : out Ada.Streams.Stream_Element_Array;
+                   Last   : out Ada.Streams.Stream_Element_Offset);
+
 private
 
    type Compress_Stream is limited new Util.Streams.Buffered.Output_Buffer_Stream with record
@@ -57,5 +77,14 @@ private
    --  Flush the stream and release the buffer.
    overriding
    procedure Finalize (Object : in out Compress_Stream);
+
+   type Decompress_Stream is limited new Util.Streams.Buffered.Input_Buffer_Stream with record
+      Stream : aliased Base.lzma_stream := Base.LZMA_STREAM_INIT;
+      Action : Base.lzma_action := Base.LZMA_RUN;
+   end record;
+
+   --  Flush the stream and release the buffer.
+   overriding
+   procedure Finalize (Object : in out Decompress_Stream);
 
 end Util.Streams.Buffered.Lzma;
