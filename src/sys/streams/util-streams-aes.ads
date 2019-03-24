a75150b657082c91d20b0f3507fc7a6523569bee
Stephane Carrez <Stephane.Carrez@gmail.com>
2019-03-24 17:17:20 +0100

    Document the AES encryption and decryption streams

--- src/sys/streams/util-streams-aes.ads
+++ src/sys/streams/util-streams-aes.ads
@@ -20,8 +20,79 @@ with Util.Encoders.AES;
 with Util.Streams.Buffered.Encoders;
 
 --  == AES Encoding Streams ==
---  The `Util.Streams.AES` package define the `Encoding_Stream` and `Decoding_Stream` to
---  encrypt and descrypt using the AES cipher.
+--  The `Util.Streams.AES` package define the `Encoding_Stream` and `Decoding_Stream` types to
+--  encrypt and decrypt using the AES cipher.  Before using these streams, you must use
+--  the `Set_Key` procedure to setup the encryption or decryption key and define the AES
+--  encryption mode to be used.  The following encryption modes are supported:
+--
+--  * AES-ECB
+--  * AES-CBC
+--  * AES-PCBC
+--  * AES-CFB
+--  * AES-OFB
+--  * AES-CTR
+--
+--  The encryption and decryption keys are represented by the `Util.Encoders.Secret_Key` limited
+--  type.  The key cannot be copied, has its content protected and will erase the memory once
+--  the instance is deleted.  The size of the encryption key defines the AES encryption level
+--  to be used:
+--
+--  * Use 16 bytes, or `Util.Encoders.AES.AES_128_Length` for AES-128,
+--  * Use 24 bytes, or `Util.Encoders.AES.AES_192_Length` for AES-192,
+--  * Use 32 bytes, or `Util.Encoders.AES.AES_256_Length` for AES-256.
+--
+--  Other key sizes will raise a pre-condition or constraint error exception.
+--  The recommended key size is 32 bytes to use AES-256.  The key could be declared as follows:
+--
+--    Key : Util.Encoders.Secret_Key
+--              (Length => Util.Encoders.AES.AES_256_Length);
+--
+--  The encryption and decryption key are initialized by using the `Util.Encoders.Create`
+--  operations or by using one of the key derivative functions provided by the
+--  `Util.Encoders.KDF` package.  A simple string password is created by using:
+--
+--    Password_Key : constant Util.Encoders.Secret_Key
+--              := Util.Encoders.Create ("mysecret");
+--
+--  Using a password key like this is not the good practice and it may be useful to generate
+--  a stronger key by using one of the key derivative function.  We will use the
+--  PBKDF2 HMAC-SHA256 with 20000 loops (see RFC 8018):
+--
+--    Util.Encoders.KDF.PBKDF2_HMAC_SHA256 (Password => Password_Key,
+--                                          Salt     => Password_Key,
+--                                          Counter  => 20000,
+--                                          Result   => Key);
+--
+--  To write a text, encrypt the content and save the file, we can chain several stream objects
+--  together.  Because they are chained, the last stream object in the chain must be declared
+--  first and the first element of the chain will be declared last.  The following declaration
+--  is used:
+--
+--      Out_Stream   : aliased Util.Streams.Files.File_Stream;
+--      Cipher       : aliased Util.Streams.AES.Encoding_Stream;
+--      Printer      : Util.Streams.Texts.Print_Stream;
+--
+--  The stream objects are chained together by using their `Initialize` procedure.
+--  The `Out_Stream` is configured to write on the `encrypted.aes` file.
+--  The `Cipher` is configured to write in the `Out_Stream` with a 32Kb buffer.
+--  The `Printer` is configured to write in the `Cipher` with a 4Kb buffer.
+--
+--      Out_Stream.Initialize (Mode => Ada.Streams.Stream_IO.In_File,
+--                             Name => "encrypted.aes");
+--      Cipher.Initialize (Output => Out_Stream'Access,
+--                         Size   => 32768);
+--      Printer.Initialize (Output => Cipher'Access,
+--                          Size   => 4096);
+--
+--  The last step before using the cipher is to configure the encryption key and modes:
+--
+--      Cipher.Set_Key (Secret => Key, Mode => Util.Encoders.AES.ECB);
+--
+--  It is now possible to write the text by using the `Printer` object:
+--
+--    Printer.Write ("Hello world!");
+--
+--
 package Util.Streams.AES is
 
    package Encoding is
