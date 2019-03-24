dea2f519ef9c2655aa60c157b1eac06946d13f71
Stephane Carrez <Stephane.Carrez@gmail.com>
2019-03-24 17:16:41 +0100

    Document the base16, base64, AES streams

--- src/sys/streams/util-streams.ads
+++ src/sys/streams/util-streams.ads
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  util-streams -- Stream utilities
---  Copyright (C) 2010, 2016, 2018 Stephane Carrez
+--  Copyright (C) 2010, 2016, 2018, 2019 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -36,7 +36,9 @@ with Ada.Streams;
 --  @include util-streams-pipes.ads
 --  @include util-streams-sockets.ads
 --  @include util-streams-raw.ads
---  @include util-streams-buffered-encoders.ads
+--  @include util-streams-base16.ads
+--  @include util-streams-base64.ads
+--  @include util-streams-aes.ads
 package Util.Streams is
 
    pragma Preelaborate;
