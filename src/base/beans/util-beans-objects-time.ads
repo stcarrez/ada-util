0b34b35ce477b31573529b60f8bf2f4eab3c0138
Stephane Carrez <Stephane.Carrez@gmail.com>
2019-07-27 10:42:21 +0200

    Add To_Object and To_Time with a Nullable_Time type

--- src/base/beans/util-beans-objects-time.ads
+++ src/base/beans/util-beans-objects-time.ads
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
---  Util.Beans.Objects.Time -- Helper conversion for Ada Calendar Time
---  Copyright (C) 2010 Stephane Carrez
+--  util-beans-objects-time  -- Helper conversion for Ada Calendar Time
+--  Copyright (C) 2010, 2019 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -17,14 +17,19 @@
 -----------------------------------------------------------------------
 
 with Ada.Calendar;
+with Util.Nullables;
 package Util.Beans.Objects.Time is
 
    --  Create an object from the given value.
    function To_Object (Value : in Ada.Calendar.Time) return Object;
 
+   function To_Object (Value : in Nullables.Nullable_Time) return Object;
+
    --  Convert the object into a time.
    --  Raises Constraint_Error if the object cannot be converter to the target type.
    function To_Time (Value : in Object) return Ada.Calendar.Time;
+
+   function To_Time (Value : in Object) return Nullables.Nullable_Time;
 
    --  Force the object to be a time.
    function Cast_Time (Value : Object) return Object;
