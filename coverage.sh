051e80c7aac16fe88201013187fe577ae2422e2e
Stephane Carrez <Stephane.Carrez@gmail.com>
2019-08-18 11:13:13 +0200

    Fix coverage script

--- coverage.sh
+++ coverage.sh
@@ -3,9 +3,11 @@ lcov --base-directory . --directory . -c -o util.cov
 bin/util_harness -xml util-aunit.xml
 lcov --base-directory . --directory . -c -o util.cov
 lcov --remove util.cov "/usr*" -o util.cov
+lcov --remove util.cov "/build*" -o util.cov
+lcov --remove util.cov "/opt*" -o util.cov
 lcov --remove util.cov "regtests*" -o util.cov
-lcov --remove util.cov ada-util/b__util_harness.adb -o util.cov
-lcov --remove util.cov ada-util/b__util_test_process.adb -o util.cov
+lcov --remove util.cov "*/b__util_harness.adb" -o util.cov
+lcov --remove util.cov "*/b__util_test_process.adb" -o util.cov
 rm -rf cover
 genhtml -o ./cover -t "test coverage" --num-spaces 4 util.cov
  
