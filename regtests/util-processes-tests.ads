2e7f57f54580869dc2d0cdbec4b9e5225a83e91a
Stephane Carrez <Stephane.Carrez@gmail.com>
2019-08-18 11:09:59 +0200

    Add Test_Input_Redirect procedure

--- regtests/util-processes-tests.ads
+++ regtests/util-processes-tests.ads
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  util-processes-tests - Test for processes
---  Copyright (C) 2011, 2016, 2018 Stephane Carrez
+--  Copyright (C) 2011, 2016, 2018, 2019 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -43,6 +43,9 @@ package Util.Processes.Tests is
 
    --  Test output file redirection.
    procedure Test_Output_Redirect (T : in out Test);
+
+   --  Test input file redirection.
+   procedure Test_Input_Redirect (T : in out Test);
 
    --  Test the Tools.Execute operation.
    procedure Test_Tools_Execute (T : in out Test);
