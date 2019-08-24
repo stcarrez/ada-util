045d37a7c9ecf302afc55606ef525a08ecea07ab
Stephane Carrez <Stephane.Carrez@gmail.com>
2019-08-24 15:26:33 +0200

    Allow to have an optional symbol prefix when we import system call symbols

--- src/sys/os-unix/util-systems-os.ads
+++ src/sys/os-unix/util-systems-os.ads
@@ -36,6 +36,8 @@ package Util.Systems.Os is
    --  The line ending separator.
    Line_Separator      : constant String := "" & ASCII.LF;
 
+   use Util.Systems.Constants;
+
    subtype Ptr is Interfaces.C.Strings.chars_ptr;
    subtype Ptr_Array is Interfaces.C.Strings.chars_ptr_array;
    type Ptr_Ptr_Array is access all Ptr_Array;
@@ -70,69 +72,69 @@ package Util.Systems.Os is
      .. +(2 ** (Standard'Address_Size - 1)) - 1;
 
    function Close (Fd : in File_Type) return Integer
-     with Import => True, Convention => C, Link_Name => "close";
+     with Import => True, Convention => C, Link_Name => SYMBOL_PREFIX & "close";
 
    function Read (Fd   : in File_Type;
                   Buf  : in System.Address;
                   Size : in Size_T) return Ssize_T
-     with Import => True, Convention => C, Link_Name => "read";
+     with Import => True, Convention => C, Link_Name => SYMBOL_PREFIX & "read";
 
    function Write (Fd   : in File_Type;
                    Buf  : in System.Address;
                    Size : in Size_T) return Ssize_T
-     with Import => True, Convention => C, Link_Name => "write";
+     with Import => True, Convention => C, Link_Name => SYMBOL_PREFIX & "write";
 
    --  System exit without any process cleaning.
    --  (destructors, finalizers, atexit are not called)
    procedure Sys_Exit (Code : in Integer)
-    with Import => True, Convention => C, Link_Name => "_exit";
+    with Import => True, Convention => C, Link_Name => SYMBOL_PREFIX & "exit";
 
    --  Fork a new process
    function Sys_Fork return Util.Processes.Process_Identifier
-     with Import => True, Convention => C, Link_Name => "fork";
+     with Import => True, Convention => C, Link_Name => SYMBOL_PREFIX & "fork";
 
    --  Fork a new process (vfork implementation)
    function Sys_VFork return Util.Processes.Process_Identifier
-     with Import => True, Convention => C, Link_Name => "fork";
+     with Import => True, Convention => C, Link_Name => SYMBOL_PREFIX & "fork";
 
    --  Execute a process with the given arguments.
    function Sys_Execvp (File : in Ptr;
                         Args : in Ptr_Array) return Integer
-     with Import => True, Convention => C, Link_Name => "execvp";
+     with Import => True, Convention => C, Link_Name => SYMBOL_PREFIX & "execvp";
 
    --  Wait for the process <b>Pid</b> to finish and return
    function Sys_Waitpid (Pid     : in Integer;
                          Status  : in System.Address;
                          Options : in Integer) return Integer
-     with Import => True, Convention => C, Link_Name => "waitpid";
+     with Import => True, Convention => C, Link_Name => SYMBOL_PREFIX & "waitpid";
 
    --  Create a bi-directional pipe
    function Sys_Pipe (Fds : in System.Address) return Integer
-     with Import => True, Convention => C, Link_Name => "pipe";
+     with Import => True, Convention => C, Link_Name => SYMBOL_PREFIX & "pipe";
 
    --  Make <b>fd2</b> the copy of <b>fd1</b>
    function Sys_Dup2 (Fd1, Fd2 : in File_Type) return Integer
-     with Import => True, Convention => C, Link_Name => "dup2";
+     with Import => True, Convention => C, Link_Name => SYMBOL_PREFIX & "dup2";
 
    --  Close a file
    function Sys_Close (Fd : in File_Type) return Integer
-     with Import => True, Convention => C, Link_Name => "close";
+     with Import => True, Convention => C, Link_Name => SYMBOL_PREFIX & "close";
 
    --  Open a file
    function Sys_Open (Path  : in Ptr;
                       Flags : in Interfaces.C.int;
                       Mode  : in Util.Systems.Types.mode_t) return File_Type
-     with Import => True, Convention => C, Link_Name => "open";
+     with Import => True, Convention => C, Link_Name => SYMBOL_PREFIX & "open";
 
    --  Change the file settings
    function Sys_Fcntl (Fd    : in File_Type;
                        Cmd   : in Interfaces.C.int;
                        Flags : in Interfaces.C.int) return Integer
-     with Import => True, Convention => C, Link_Name => "fcntl";
+     with Import => True, Convention => C, Link_Name => SYMBOL_PREFIX & "fcntl";
 
    function Sys_Kill (Pid : in Integer;
                       Signal : in Integer) return Integer
-     with Import => True, Convention => C, Link_Name => "kill";
+     with Import => True, Convention => C, Link_Name => SYMBOL_PREFIX & "kill";
 
    function Sys_Stat (Path : in Ptr;
                       Stat : access Util.Systems.Types.Stat_Type) return Integer
@@ -146,40 +148,40 @@ package Util.Systems.Os is
                        Offset : in Util.Systems.Types.off_t;
                        Mode   : in Util.Systems.Types.Seek_Mode)
                        return Util.Systems.Types.off_t
-     with Import => True, Convention => C, Link_Name => "lseek";
+     with Import => True, Convention => C, Link_Name => SYMBOL_PREFIX & "lseek";
 
    function Sys_Ftruncate (Fs : in File_Type;
                            Length : in Util.Systems.Types.off_t) return Integer
-     with Import => True, Convention => C, Link_Name => "ftruncate";
+     with Import => True, Convention => C, Link_Name => SYMBOL_PREFIX & "ftruncate";
 
    function Sys_Truncate (Path  : in Ptr;
                           Length : in Util.Systems.Types.off_t) return Integer
-     with Import => True, Convention => C, Link_Name => "truncate";
+     with Import => True, Convention => C, Link_Name => SYMBOL_PREFIX & "truncate";
 
    function Sys_Fchmod (Fd   : in File_Type;
                         Mode : in Util.Systems.Types.mode_t) return Integer
-     with Import => True, Convention => C, Link_Name => "fchmod";
+     with Import => True, Convention => C, Link_Name => SYMBOL_PREFIX & "fchmod";
 
    --  Change permission of a file.
    function Sys_Chmod (Path  : in Ptr;
                        Mode  : in Util.Systems.Types.mode_t) return Integer
-     with Import => True, Convention => C, Link_Name => "chmod";
+     with Import => True, Convention => C, Link_Name => SYMBOL_PREFIX & "chmod";
 
    --  Change working directory.
    function Sys_Chdir (Path : in Ptr) return Integer
-     with Import => True, Convention => C, Link_Name => "chdir";
+     with Import => True, Convention => C, Link_Name => SYMBOL_PREFIX & "chdir";
 
    --  Rename a file (the Ada.Directories.Rename does not allow to use
    --  the Unix atomic file rename!)
    function Sys_Rename (Oldpath  : in Ptr;
                         Newpath  : in Ptr) return Integer
-     with Import => True, Convention => C, Link_Name => "rename";
+     with Import => True, Convention => C, Link_Name => SYMBOL_PREFIX & "rename";
 
    --  Libc errno.  The __get_errno function is provided by the GNAT runtime.
    function Errno return Integer
-     with Import => True, Convention => C, Link_Name => "__get_errno";
+     with Import => True, Convention => C, Link_Name => SYMBOL_PREFIX & "_get_errno";
 
    function Strerror (Errno : in Integer) return Interfaces.C.Strings.chars_ptr
-     with Import => True, Convention => C, Link_Name => "strerror";
+     with Import => True, Convention => C, Link_Name => SYMBOL_PREFIX & "strerror";
 
 end Util.Systems.Os;
