bdddb8435865a2212fa1480963b3ef7de0d2d1af
Stephane Carrez <Stephane.Carrez@gmail.com>
2020-02-15 14:16:31 +0100

    Revert to avoid using --include and --exclude which is supported only by recent lcov

--- coverage.sh
+++ coverage.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 NAME=util.cov
-lcov --quiet --base-directory . --directory . -c --include "*/ada-util/src/*" -o $NAME
+lcov --quiet --base-directory . --directory . -c -o $NAME
+lcov --quiet --remove $NAME "/usr*" -o $NAME
+lcov --quiet --remove $NAME "/build*" -o $NAME
+lcov --quiet --remove $NAME "/opt*" -o $NAME
+lcov --quiet --remove $NAME "*/regtests*" -o $NAME
 lcov --quiet --remove $NAME "*/src/tests/*" -o $NAME
+lcov --quiet --remove $NAME "*/b__util_harness.adb" -o $NAME
+lcov --quiet --remove $NAME "*/b__util_test_process.adb" -o $NAME
 rm -rf cover
 genhtml --quiet -o ./cover -t "test coverage" --num-spaces 4 $NAME
-
  
