cc237801ca393d7b0d95db891e831ad831f28fdd
Stephane Carrez <Stephane.Carrez@gmail.com>
2020-09-12 18:22:16 +0200

    New package to provide more unit tests for dates

---
+++ regtests/util-dates-tests.ads
@@ -0,0 +1,32 @@
+-----------------------------------------------------------------------
+--  util-dates-tests - Test for dates
+--  Copyright (C) 2018, 2020 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+with Util.Tests;
+package Util.Dates.Tests is
+
+   procedure Add_Tests (Suite : in Util.Tests.Access_Test_Suite);
+
+   type Test is new Util.Tests.Test with null record;
+
+   --  Test converting a date in ISO8601.
+   procedure Test_ISO8601_Image (T : in out Test);
+
+   --  Test converting a string in ISO8601 into a date.
+   procedure Test_ISO8601_Value (T : in out Test);
+
+end Util.Dates.Tests;
