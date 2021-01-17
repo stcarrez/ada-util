f4208d7065d4e569e154178f79708580c51a2bf4
Stephane Carrez <Stephane.Carrez@gmail.com>
2021-01-17 18:46:39 +0100

    Add Test_Console_Appender procedure

--- regtests/util-log-tests.ads
+++ regtests/util-log-tests.ads
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  log.tests -- Unit tests for loggers
---  Copyright (C) 2009, 2010, 2011, 2015 Stephane Carrez
+--  Copyright (C) 2009, 2010, 2011, 2015, 2021 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -28,6 +28,7 @@ package Util.Log.Tests is
    procedure Test_Log (T : in out Test);
    procedure Test_File_Appender (T : in out Test);
    procedure Test_List_Appender (T : in out Test);
+   procedure Test_Console_Appender (T : in out Test);
 
    --  Test file appender with different modes.
    procedure Test_File_Appender_Modes (T : in out Test);
