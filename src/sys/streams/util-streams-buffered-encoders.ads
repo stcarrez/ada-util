90dedb2fd764e2ec03a95d0ff1bb37067b744beb
Stephane Carrez <Stephane.Carrez@gmail.com>
2021-01-31 18:03:55 +0100

    Refactor the Util.Stream.Buffered stream - allow to use the target stream with Read (pull model) and Write (push model)

--- src/sys/streams/util-streams-buffered-encoders.ads
+++ src/sys/streams/util-streams-buffered-encoders.ads
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  util-streams-encoders -- Streams with encoding and decoding capabilities
---  Copyright (C) 2017, 2019 Stephane Carrez
+--  Copyright (C) 2017, 2019, 2021 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -41,17 +41,26 @@ package Util.Streams.Buffered.Encoders is
    --  The <b>Encoding_Stream</b> is an output stream which uses an encoder to
    --  transform the data before writing it to the output.  The transformer can
    --  change the data by encoding it in Base64, Base16 or encrypting it.
-   type Encoder_Stream is limited new Util.Streams.Buffered.Output_Buffer_Stream with record
+   type Encoder_Stream is limited new Util.Streams.Buffered.Input_Output_Buffer_Stream
+     with record
       Transform   : Encoder;
       Flushed     : Boolean := False;
    end record;
 
+   --  Initialize the stream with a buffer of <b>Size</b> bytes.
+   procedure Initialize (Stream  : in out Encoder_Stream;
+                         Size    : in Positive);
+
    --  Initialize the stream to write on the given stream.
    --  An internal buffer is allocated for writing the stream.
-   overriding
-   procedure Initialize (Stream  : in out Encoder_Stream;
-                         Output  : access Output_Stream'Class;
-                         Size    : in Positive);
+   procedure Produces (Stream  : in out Encoder_Stream;
+                       Output  : access Output_Stream'Class;
+                       Size    : in Positive);
+
+   --  Initialize the stream to read the given streams.
+   procedure Consumes (Stream  : in out Encoder_Stream;
+                       Input   : access Input_Stream'Class;
+                       Size    : in Positive);
 
    --  Close the sink.
    overriding
@@ -62,6 +71,13 @@ package Util.Streams.Buffered.Encoders is
    procedure Write (Stream : in out Encoder_Stream;
                     Buffer : in Ada.Streams.Stream_Element_Array);
 
+   --  Read into the buffer as many bytes as possible and return in
+   --  `last` the position of the last byte read.
+   overriding
+   procedure Read (Stream : in out Encoder_Stream;
+                   Into   : out Ada.Streams.Stream_Element_Array;
+                   Last   : out Ada.Streams.Stream_Element_Offset);
+
    --  Flush the buffer by writing on the output stream.
    --  Raises Data_Error if there is no output stream.
    overriding
@@ -69,5 +85,9 @@ package Util.Streams.Buffered.Encoders is
 
    overriding
    procedure Finalize (Stream : in out Encoder_Stream);
+
+   --  Fill the buffer by reading the input stream.
+   --  Raises Data_Error if there is no input stream;
+   procedure Fill (Stream : in out Encoder_Stream);
 
 end Util.Streams.Buffered.Encoders;
