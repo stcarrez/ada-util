a666dcd249f936a34a7e02ad4981beb8112d87ba
Stephane Carrez <Stephane.Carrez@gmail.com>
2022-02-13 16:00:39 +0100

    Move Simple_Format procedure outside of Util.Dates.Formats to reduce dependencies

--- src/base/dates/util-dates-formats.adb
+++ src/base/dates/util-dates-formats.adb
@@ -466,46 +466,6 @@ package body Util.Dates.Formats is
       return To_String (Result);
    end Format;
 
-   --  ------------------------------
-   --  Format the date passed in `Date` with a simple format pattern.
-   --  The pattern is composed of minimalist sequences that are replaced by
-   --  values, unrecognized characters are passed as is:
-   --  YYYY : year     MM : month     dd: day    HH: hour   mm: minute   ss: second
-   --  ------------------------------
-   function Simple_Format (Pattern : in String;
-                           Date    : in Ada.Calendar.Time) return String is
-      Pos    : Natural := Pattern'First;
-      TM     : Date_Record;
-      Result : Unbounded_String;
-   begin
-      Split (TM, Date);
-      while Pos <= Pattern'Last loop
-         if Pos + 3 <= Pattern'Last and then Pattern (Pos .. Pos + 3) = "YYYY" then
-            Pos := Pos + 4;
-            Append_Number (Result, Natural (TM.Year), Padding => '0', Length => 4);
-         elsif Pos + 1 <= Pattern'Last and then Pattern (Pos .. Pos + 1) = "MM" then
-            Pos := Pos + 2;
-            Append_Number (Result, Natural (TM.Month), Padding => '0', Length => 2);
-         elsif Pos + 1 <= Pattern'Last and then Pattern (Pos .. Pos + 1) = "dd" then
-            Pos := Pos + 2;
-            Append_Number (Result, Natural (TM.Month_Day), Padding => '0', Length => 2);
-         elsif Pos + 1 <= Pattern'Last and then Pattern (Pos .. Pos + 1) = "HH" then
-            Pos := Pos + 2;
-            Append_Number (Result, Natural (TM.Hour), Padding => '0', Length => 2);
-         elsif Pos + 1 <= Pattern'Last and then Pattern (Pos .. Pos + 1) = "mm" then
-            Pos := Pos + 2;
-            Append_Number (Result, Natural (TM.Minute), Padding => '0', Length => 2);
-         elsif Pos + 1 <= Pattern'Last and then Pattern (Pos .. Pos + 1) = "ss" then
-            Pos := Pos + 2;
-            Append_Number (Result, Natural (TM.Second), Padding => '0', Length => 2);
-         else
-            Append (Result, Pattern (Pos));
-            Pos := Pos + 1;
-         end if;
-      end loop;
-      return To_String (Result);
-   end Simple_Format;
-
    procedure Parse (Date      : in String;
                     Pattern   : in String;
                     Bundle    : in Util.Properties.Manager'Class;
