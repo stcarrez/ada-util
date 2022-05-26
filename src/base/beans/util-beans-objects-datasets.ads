9cf3d8ef86d7de1e00e755fcb77efdcd8e15de14
Stephane Carrez <Stephane.Carrez@gmail.com>
2022-05-26 10:13:05 +0200

    Add some documentation Implement the Iterator_Bean interface to allow iterating over the dataset and its columns

--- src/base/beans/util-beans-objects-datasets.ads
+++ src/base/beans/util-beans-objects-datasets.ads
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
---  Util.Beans.Objects.Datasets -- Datasets
---  Copyright (C) 2013, 2018 Stephane Carrez
+--  util-beans-objects-datasets -- Datasets
+--  Copyright (C) 2013, 2018, 2022 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -20,6 +20,7 @@ with Ada.Finalization;
 with Ada.Containers.Indefinite_Hashed_Maps;
 
 with Util.Beans.Basic;
+with Util.Beans.Objects.Iterators;
 
 --  == Datasets ==
 --  The `Datasets` package implements the `Dataset` list bean which
@@ -28,14 +29,48 @@ with Util.Beans.Basic;
 --  Each row defines a `Bean` instance and allows to access each column value.
 --  Each column is associated with a unique name.  The row `Bean` allows to
 --  get or set the column by using the column name.
+--
+--     with Util.Beans.Objects.Datasets;
+--     ...
+--        Set : Util.Beans.Objects.Datasets.Dataset_Access
+--            := new Util.Beans.Objects.Datasets.Dataset;
+--
+--  After creation of the dataset instance, the first step is to define
+--  the columns that composed the list.  This is done by using the `Add_Column`
+--  procedure:
+--
+--     Set.Add_Column ("name");
+--     Set.Add_Column ("email");
+--     Set.Add_Column ("age");
+--
+--  To populate the dataset, the package only provide the `Append` procedure
+--  which adds a new row and calls a procedure whose job is to fill the columns
+--  of the new row.  The procedure gets the row as an array of `Object`:
+--
+--     procedure Fill (Row : in out Util.Beans.Objects.Object_Array) is
+--     begin
+--        Row (Row'First) := To_Object (String '("Yoda"));
+--        Row (Row'First + 1) := To_Object (String '("Yoda@Dagobah"));
+--        Row (Row'First + 2) := To_Object (Integer (900));
+--     end Fill;
+--
+--     Set.Append (Fill'Access);
+--
+--  The dataset instance is converted to an `Object` by using the `To_Object`
+--  function.  Note that the default behavior of `To_Object` is to take
+--  the ownershiup of the object and hence it will be released automatically.
+--
+--     List : Util.Beans.Objects.Object
+--        := Util.Beans.Objects.To_Object (Set);
+--
 package Util.Beans.Objects.Datasets is
+
+   subtype Iterator_Bean is Util.Beans.Objects.Iterators.Iterator_Bean;
 
    Invalid_State : exception;
 
-   --  An array of objects.
-   type Object_Array is array (Positive range <>) of Object;
-
-   type Dataset is new Util.Beans.Basic.List_Bean with private;
+   type Dataset is new Util.Beans.Basic.List_Bean and Iterator_Bean with private;
+   type Dataset_Access is access all Dataset'Class;
 
    --  Get the number of elements in the list.
    overriding
@@ -55,6 +90,14 @@ package Util.Beans.Objects.Datasets is
    overriding
    function Get_Value (From : in Dataset;
                        Name : in String) return Util.Beans.Objects.Object;
+
+   --  Get an iterator to iterate starting with the first element.
+   overriding
+   function First (From : in Dataset) return Iterators.Proxy_Iterator_Access;
+
+   --  Get an iterator to iterate starting with the last element.
+   overriding
+   function Last (From : in Dataset) return Iterators.Proxy_Iterator_Access;
 
    --  Append a row in the dataset and call the fill procedure to populate
    --  the row content.
@@ -85,7 +128,7 @@ private
                                                  Equivalent_Keys => "=",
                                                  "="             => "=");
 
-   type Row is new Util.Beans.Basic.Bean with record
+   type Row is new Util.Beans.Basic.Bean and Iterator_Bean with record
       Data : Object_Array_Access;
       Map  : access Dataset_Map.Map;
    end record;
@@ -105,7 +148,16 @@ private
                         Name  : in String;
                         Value : in Util.Beans.Objects.Object);
 
-   type Dataset is new Ada.Finalization.Controlled and Util.Beans.Basic.List_Bean with record
+   --  Get an iterator to iterate starting with the first element.
+   overriding
+   function First (From : in Row) return Iterators.Proxy_Iterator_Access;
+
+   --  Get an iterator to iterate starting with the last element.
+   overriding
+   function Last (From : in Row) return Iterators.Proxy_Iterator_Access;
+
+   type Dataset is new Ada.Finalization.Controlled
+     and Util.Beans.Basic.List_Bean and Iterator_Bean with record
       Data        : Dataset_Array_Access;
       Count       : Natural := 0;
       Columns     : Natural := 0;
@@ -122,5 +174,51 @@ private
    --  Release the dataset storage.
    overriding
    procedure Finalize (Set : in out Dataset);
+
+   type Dataset_Iterator is new Iterators.Proxy_Iterator with record
+      Current     : aliased Row;
+      Current_Pos : Natural := 0;
+      Row         : Util.Beans.Objects.Object;
+   end record;
+   type Dataset_Iterator_Access is access all Dataset_Iterator;
+
+   overriding
+   procedure Initialize (Iter : in out Dataset_Iterator);
+
+   overriding
+   function Has_Element (Iter : in Dataset_Iterator) return Boolean;
+
+   overriding
+   procedure Next (Iter : in out Dataset_Iterator);
+
+   overriding
+   procedure Previous (Iter : in out Dataset_Iterator);
+
+   overriding
+   function Element (Iter : in Dataset_Iterator) return Object;
+
+   type Row_Iterator is new Iterators.Proxy_Map_Iterator with record
+      Pos  : Dataset_Map.Cursor;
+      Data : Object_Array_Access;
+   end record;
+   type Row_Iterator_Access is access all Row_Iterator;
+
+   overriding
+   procedure Initialize (Iter : in out Row_Iterator);
+
+   overriding
+   function Has_Element (Iter : in Row_Iterator) return Boolean;
+
+   overriding
+   procedure Next (Iter : in out Row_Iterator);
+
+   overriding
+   procedure Previous (Iter : in out Row_Iterator);
+
+   overriding
+   function Element (Iter : in Row_Iterator) return Object;
+
+   overriding
+   function Key (Iter : in Row_Iterator) return String;
 
 end Util.Beans.Objects.Datasets;
