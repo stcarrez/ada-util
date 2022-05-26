58ed772731764e6c69ea82df1fd5308699febc91
Stephane Carrez <Stephane.Carrez@gmail.com>
2022-05-26 10:14:58 +0200

    Add some documentation and implement the Iterator_Bean

--- src/base/beans/util-beans-objects-maps.ads
+++ src/base/beans/util-beans-objects-maps.ads
@@ -19,29 +19,38 @@
 with Ada.Containers.Indefinite_Hashed_Maps;
 with Ada.Strings.Hash;
 with Util.Beans.Basic;
+with Util.Beans.Objects.Iterators;
 
 --  == Object maps ==
 --  The `Util.Beans.Objects.Maps` package provides a map of objects with a `String`
---  as index.  This allows to associated names to objects.
+--  as key.  This allows to associated names to objects.
 --  To create an instance of the map, it is possible to use the `Create` function
 --  as follows:
 --
---    Person : Util.Beans.Objects.Object := Util.Beans.Objects.Maps.Create;
+--    with Util.Beans.Objects.Maps;
+--    ...
+--       Person : Util.Beans.Objects.Object := Util.Beans.Objects.Maps.Create;
 --
 --  Then, it becomes possible to populate the map with objects by using
 --  the `Set_Value` procedure as follows:
 --
---    Util.Beans.Objects.Set_Value (Person, "name", To_Object (Name));
---    Util.Beans.Objects.Set_Value (Person, "last_name", To_Object (Last_Name));
---    Util.Beans.Objects.Set_Value (Person, "age", To_Object (Age));
+--    Util.Beans.Objects.Set_Value (Person, "name",
+--                                  To_Object (Name));
+--    Util.Beans.Objects.Set_Value (Person, "last_name",
+--                                  To_Object (Last_Name));
+--    Util.Beans.Objects.Set_Value (Person, "age",
+--                                  To_Object (Age));
 --
 --  Getting a value from the map is done by using the `Get_Value` function:
 --
 --    Name : Util.Beans.Objects.Object := Get_Value (Person, "name");
 --
 --  It is also possible to iterate over the values of the map by using
---  the `Iterate` procedure.
+--  the `Iterate` procedure or by using the iterator support provided by
+--  the `Util.Beans.Objects.Iterators` package.
 package Util.Beans.Objects.Maps is
+
+   subtype Iterator_Bean is Util.Beans.Objects.Iterators.Iterator_Bean;
 
    package Maps is
      new Ada.Containers.Indefinite_Hashed_Maps (Key_Type        => String,
@@ -83,20 +92,30 @@ package Util.Beans.Objects.Maps is
    --  ------------------------------
    --  The <b>Map_Bean</b> is a map of objects that also exposes the <b>Bean</b> interface.
    --  This allows the map to be available and accessed from an Object instance.
-   type Map_Bean is new Maps.Map and Util.Beans.Basic.Bean with private;
+   type Map_Bean is new Maps.Map and Util.Beans.Basic.Bean and Iterator_Bean with private;
    type Map_Bean_Access is access all Map_Bean'Class;
 
    --  Get the value identified by the name.
    --  If the name cannot be found, the method should return the Null object.
+   overriding
    function Get_Value (From : in Map_Bean;
                        Name : in String) return Object;
 
    --  Set the value identified by the name.
    --  If the map contains the given name, the value changed.
    --  Otherwise name is added to the map and the value associated with it.
+   overriding
    procedure Set_Value (From  : in out Map_Bean;
                         Name  : in String;
                         Value : in Object);
+
+   --  Get an iterator to iterate starting with the first element.
+   overriding
+   function First (From : in Map_Bean) return Iterators.Proxy_Iterator_Access;
+
+   --  Get an iterator to iterate starting with the last element.
+   overriding
+   function Last (From : in Map_Bean) return Iterators.Proxy_Iterator_Access;
 
    --  Create an object that contains a <tt>Map_Bean</tt> instance.
    function Create return Object;
@@ -108,6 +127,27 @@ package Util.Beans.Objects.Maps is
 
 private
 
-   type Map_Bean is new Maps.Map and Util.Beans.Basic.Bean with null record;
+   type Map_Bean is new Maps.Map and Util.Beans.Basic.Bean
+     and Iterator_Bean with null record;
+
+   type Map_Iterator is new Iterators.Proxy_Map_Iterator with record
+      Pos : Cursor;
+   end record;
+   type Map_Iterator_Access is access all Map_Iterator;
+
+   overriding
+   function Has_Element (Iter : in Map_Iterator) return Boolean;
+
+   overriding
+   procedure Next (Iter : in out Map_Iterator);
+
+   overriding
+   procedure Previous (Iter : in out Map_Iterator);
+
+   overriding
+   function Element (Iter : in Map_Iterator) return Object;
+
+   overriding
+   function Key (Iter : in Map_Iterator) return String;
 
 end Util.Beans.Objects.Maps;
