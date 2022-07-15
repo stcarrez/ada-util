a5ba0f4ba9dc8093d47fd955ac9da8a37433d5eb
Stephane Carrez <Stephane.Carrez@gmail.com>
2022-07-15 17:07:31 +0200

    Update missing with clause

--- src/sys/http/util-http-headers.ads
+++ src/sys/http/util-http-headers.ads
@@ -15,6 +15,7 @@
 --  See the License for the specific language governing permissions and
 --  limitations under the License.
 -----------------------------------------------------------------------
+with Util.Dates.RFC7231;
 
 package Util.Http.Headers is
 
