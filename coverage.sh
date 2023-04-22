277857ee3e14465944d9d98a887f6d004740d534
Stephane Carrez <Stephane.Carrez@gmail.com>
2023-04-22 15:48:06 +0200

    Cleanup the script

--- coverage.sh
+++ coverage.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 NAME=util.cov
-lcov --quiet --base-directory . --directory . -c -o $NAME
-lcov --quiet --remove $NAME "/usr*" -o $NAME
-lcov --quiet --remove $NAME "/build*" -o $NAME
-lcov --quiet --remove $NAME "/opt*" -o $NAME
-lcov --quiet --remove $NAME "*/regtests*" -o $NAME
-lcov --quiet --remove $NAME "*/src/tests/*" -o $NAME
-lcov --quiet --remove $NAME "*/b__util_harness.adb" -o $NAME
-lcov --quiet --remove $NAME "*/b__util_test_process.adb" -o $NAME
+lcov --quiet --base-directory . --directory . \
+   --no-external \
+   --exclude '*/b__*.adb' \
+   --exclude '*/samples/*' \
+   --exclude '*/regtests*' \
+   --exclude '*/src/tests/*' -c -o $NAME
 rm -rf cover
 genhtml --quiet -o ./cover -t "test coverage" --num-spaces 4 $NAME
  
